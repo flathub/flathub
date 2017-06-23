9cfb90bfdfb0e22c2619d40019c1e8097410e6de
Valentin David <valentin.david@gmail.com>
2017-06-23 22:52:56 +0200

    License splitelf

--- splitelf.c
+++ splitelf.c
@@ -1,3 +1,21 @@
+/*
+    splitelf: Extract trailing data from an ELF file.
+    Copyright (C) 2017  Valentin David
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
