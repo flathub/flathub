2f94e8f07aef411e9a86be0c0e8c9603d0e7629d
钊哥 <amtlib-dot-dll@users.noreply.github.com>
2018-02-26 19:56:50 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,11 +8,9 @@ yarn config set yarn-offline-mirror "$(realpath ./yarn-mirror)"
 tar -xzvf yarn-mirror/vscode-ripgrep-0.7.1-patch.0.1.tgz
 mv package vscode-ripgrep-0.7.1-patch.0.1
 pushd vscode-ripgrep-0.7.1-patch.0.1
-    mv _node_modules node_modules
     yarn link
-    echo > dist/postinstall.js
     mkdir bin
-    unzip ../misc/ripgrep-0.7.1-patch.1-linux-$(node -e 'console.log(process.arch)').zip rg -d bin/
+    unzip ../misc/ripgrep-0.7.1-patch.0-linux-$(node -e 'console.log(process.arch)').zip rg -d bin/
     chmod 755 bin/rg
 popd
 tar -xzvf yarn-mirror/vscode-1.0.1.tgz
@@ -35,9 +33,8 @@ pushd vscode
     sed -i "s/'vscode\-colorize\-tests',//" build/gulpfile.vscode.js
     sed -i "s/'vscode\-api\-tests',//" build/npm/postinstall.js
     sed -i "s/'vscode\-colorize\-tests',//" build/npm/postinstall.js
-    while true; do
-        npm_config_tarball="$(realpath ../misc/iojs-v1.7.9.tar.gz)" yarn install --offline --verbose --frozen-lockfile && break
-    done
+    python2 /app/lib/node_modules/npm/node_modules/node-gyp/gyp/gyp_main.py --help || true
+    npm_config_tarball="$(realpath ../misc/iojs-v1.7.9.tar.gz)" yarn install --offline --verbose --frozen-lockfile
     rm node_modules/vscode-ripgrep
     cp -r ../vscode-ripgrep-0.7.1-patch.0.1 node_modules/vscode-ripgrep
     echo "/// <reference types='@types/node'/>" > extensions/emmet/src/typings/refs.d.ts
