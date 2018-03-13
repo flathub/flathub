535f55102760a8e568f3329eb27147697471ea62
Alexander Wilms <f.alexander.wilms@gmail.com>
2018-03-13 22:16:58 +0100

    Fixes

--- flightgear.sh
+++ flightgear.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-FG_ROOT=/app/share/flightgear LD_LIBRARY_PATH=/app/lib64 FG_AIRCRAFT=~/.fgfs/Aircraft/ FG_SCENERY=~/.fgfs/Scenery fgfs "$@"
+FG_ROOT=/app/share/flightgear FG_AIRCRAFT=$XDG_DATA_HOME/fgfs/Aircraft/ FG_SCENERY=$XDG_DATA_HOME/fgfs/Scenery fgfs "$@"
