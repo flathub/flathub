beba088b73e69071e1fa6695f90d57f5c49a6baf
Peter Eszlari <peter.eszlari@gmail.com>
2018-05-10 03:41:23 +0200

    use java extension again, the bundled jre is old

--- jd-wrapper.sh
+++ jd-wrapper.sh
@@ -5,9 +5,10 @@ JDSETUP=$XDG_CACHE_HOME/JD2Setup.sh
 
 if [ ! -f $JDDIR/JDownloader.jar ]; then
     install -Dm755 /app/extra/JD2Setup.sh $JDSETUP
-    $JDSETUP -q -dir $JDDIR | zenity --progress --text="Installing JDownloader" --pulsate --no-cancel --auto-close
-    rm $JDSETUP
+    $JDSETUP -q -dir $JDDIR/tmp | zenity --progress --text="Installing JDownloader" --pulsate --no-cancel --auto-close
+    mv $JDDIR/tmp/JDownloader.jar $JDDIR
+    rm -rf $JDSETUP $JDDIR/tmp
     zenity --info --text "Download directory: $HOME/JDownloader" --no-wrap --title "JDownloader"
 fi
 
-$JDDIR/JDownloader2
+java -jar $JDDIR/JDownloader.jar
