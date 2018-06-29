d5df5766e018c6fb4abfaab901f1475bca4874c2
scx <scx.mail@gmail.com>
2018-06-30 01:11:40 +0200

    widelands module: beautify the script

--- widelands_edit_appdata.bash
+++ widelands_edit_appdata.bash
@@ -6,9 +6,9 @@ echo 'edit appdata';
 xmlstarlet_home='/app/opt/xmlstarlet';
 xmlstarlet_bin="${xmlstarlet_home}/bin";
 if [[ -n "${PATH}" ]]; then
-    PATH="${PATH}:${xmlstarlet_bin}";
+  PATH="${PATH}:${xmlstarlet_bin}";
 else
-    PATH="${xmlstarlet_bin}";
+  PATH="${xmlstarlet_bin}";
 fi;
 export PATH;
 xmlstarlet ed --inplace -d '/component/releases/release[position()>1]' "${app_name}.appdata.xml";
