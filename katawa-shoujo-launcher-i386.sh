4853c37dde08a14da6ce3f362a41ee5760fba959
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2018-07-16 11:20:34 +0200

    Remove unneeded wrappers

--- katawa-shoujo-launcher-i386.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec "/app/lib/linux-i686/python" "-EO" "/app/Katawa Shoujo.py" "$@"
--- katawa-shoujo-launcher-x86_64.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec "/app/lib/linux-x86_64/python" "-EO" "/app/Katawa Shoujo.py" "$@"
