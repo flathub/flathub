3dc5b6f6d42e724418c14bbb6938cecede731e58
Konstantinos Sideris <sideris.konstantin@gmail.com>
2018-08-02 20:52:50 +0300

    Remove file renaming from the install script

--- install-resources.sh
+++ install-resources.sh
@@ -8,15 +8,9 @@ install -dm755 /app/share/pixmaps/
 install -dm755 /app/share/applications/
 install -Dm755 build/nheko /app/bin/nheko
 install -Dm755 resources/nheko-256.png /app/share/pixmaps/${APP_ID}.png
-install -Dm755 resources/nheko.desktop /app/share/applications/${APP_ID}.desktop
-
-install -Dm755 nheko.appdata.xml /app/share/metainfo/${APP_ID}.appdata.xml
 
 for icon_size in 16 32 48 64 128 256 512; do
     icon_dir=/app/share/icons/hicolor/${icon_size}x${icon_size}/apps
     install -d $icon_dir
     install -m644 resources/nheko-${icon_size}.png $icon_dir/${APP_ID}.png
 done
-
-desktop-file-edit --set-icon=${APP_ID} /app/share/applications/${APP_ID}.desktop
-desktop-file-validate /app/share/applications/${APP_ID}.desktop
