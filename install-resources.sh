1e9bb23b5aa296dbafdaa060b4c44faf8a0128cf
Konstantinos Sideris <sideris.konstantin@gmail.com>
2018-08-02 18:56:20 +0300

    Remove unnecessary copies

--- install-resources.sh
+++ install-resources.sh
@@ -10,11 +10,6 @@ install -Dm755 build/nheko /app/bin/nheko
 install -Dm755 resources/nheko-256.png /app/share/pixmaps/${APP_ID}.png
 install -Dm755 resources/nheko.desktop /app/share/applications/${APP_ID}.desktop
 
-cp /usr/local/lib/*.so /app/lib/
-cp /usr/local/lib/libolm* /app/lib/
-cp /usr/lib/libsodium* /app/lib/
-cp /usr/lib/libboost* /app/lib/
-
 install -Dm755 nheko.appdata.xml /app/share/metainfo/${APP_ID}.appdata.xml
 
 for icon_size in 16 32 48 64 128 256 512; do
