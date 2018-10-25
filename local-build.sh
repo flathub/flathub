0f9d75616fb777c71bcd0016724b426cea15ceb6
Bastien Nocera <hadess@hadess.net>
2018-10-25 17:30:01 +0200

    Add script to automate a local build

---
+++ local-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+rm -f org.gnome.Totem.flatpak
+rm -rf _build ; mkdir _build
+rm -rf _repo ; mkdir _repo
+
+flatpak-builder --ccache --force-clean _build org.gnome.Totem.json --repo=_repo
+flatpak build-bundle _repo org.gnome.Totem.flatpak org.gnome.Totem master
+
