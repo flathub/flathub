6c8fc61ab214b90587e88d4b4c3193acba993804
Mathieu Velten <matmaul@gmail.com>
2018-11-20 23:17:57 +0100

    MakeMKV plugin for VLC
    Provides MakeMKV features for direct Blu-ray playback in VLC.

---
+++ makemkv-env.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export LIBAACS_PATH=/app/share/vlc/extra/makemkv/lib/libmmbd
+export LIBBDPLUS_PATH=/app/share/vlc/extra/makemkv/lib/libmmbd
+export MAKEMKVCON=/app/share/vlc/extra/makemkv/bin/makemkvcon
