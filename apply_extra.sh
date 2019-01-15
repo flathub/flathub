e7a9b98814f92204974170b99f34be588c8e5897
gasinvein <gasinvein@gmail.com>
2019-01-15 19:33:57 +0300

    Update apply_extra.sh - Exit on error - Fix archive filename - Place WPS into subdir - Use dot separator instead of dash - Remove appurl.desktop

--- apply_extra.sh
+++ apply_extra.sh
@@ -1,20 +1,24 @@
-#!/bin/bash -x
-tar -xJf wps-office.tar.xz --strip-components=1
+#!/bin/bash
+set -e
 
-mkdir -p export/share
-mv resource/* export/share/
+mkdir -p wps-office export/share
+
+tar -xJf wps.tar.xz --strip-components=1 -C wps-office
+
+cp -ax wps-office/resource/{icons,applications,mime} export/share/
+rm export/share/applications/appurl.desktop
 
 for f in export/share/{icons/hicolor/*/*,applications,mime/packages}/wps-office-*.*; do
-    mv "$f" "${f/wps-office-/com.wps.Office-}";
+    mv "$f" "${f/wps-office-/com.wps.Office.}";
 done
-sed -i 's|/opt/kingsoft/wps-office|/app/extra|g' -i {wps,wpp,et}
-sed -i 's|Exec=/usr/bin/|Exec=|g' -i export/share/applications/com.wps.Office-*.desktop
-sed -i 's/Icon=wps-office/Icon=com.wps.Office/' export/share/applications/com.wps.Office-*.desktop
-sed -i 's/generic-icon name="wps-office-/icon name="com.wps.Office-/g' export/share/mime/packages/com.wps.Office-*.xml
+sed -i 's|/opt/kingsoft/wps-office|/app/extra/wps-office|g' -i wps-office/{wps,wpp,et}
+sed -i 's|Exec=/usr/bin/|Exec=|g' -i export/share/applications/com.wps.Office.*.desktop
+sed -i 's/Icon=wps-office-/Icon=com.wps.Office./' export/share/applications/com.wps.Office.*.desktop
+sed -i 's/generic-icon name="wps-office-/icon name="com.wps.Office./g' export/share/mime/packages/com.wps.Office.*.xml
 
 for l in /app/share/wps/office6/mui/*; do
     d=$(basename $l)
-    test -d office6/mui/$d || ln -sr /app/share/wps/office6/mui/$d office6/mui/$d
+    test -d wps-office/office6/mui/$d || ln -sr /app/share/wps/office6/mui/$d wps-office/office6/mui/$d
 done
 
-rm wps-office.tar.xz
+rm wps.tar.xz
