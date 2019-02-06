9939a8b008fc8c1b49d4d80e41ccbcb3769837e3
Ryan Gonzalez <rymg19@gmail.com>
2019-02-05 19:20:51 -0600

    Patch Unity's incorrect disk space detection

---
+++ apply_extra.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+chmod +x *.AppImage
+./UnityHubSetup.AppImage --appimage-extract
+mv squashfs-root/* .
+rmdir squashfs-root
+# asar is a binary archive format, so the replacement string *must* be the same length
+# as the original.
+sed -i -f - resources/app.asar <<'EOF'
+s|${os\.homedir()}/\.local/share|${process.env.UNITY_DATADIR}|g;
+s|path\.parse(folder)\.root|path.parse(folder).dir |g;
+EOF
+rm AppRun
+mkdir -p export/share/applications
+sed '/X-AppImage/d;s/Exec=AppRun/Exec=start-unityhub/;s/Icon=unityhub/Icon=com.unity.UnityHub/' \
+  unityhub.desktop > export/share/applications/com.unity.UnityHub.desktop
+install -Dm 644 unityhub.png export/share/icons/hicolor/48x48/apps/com.unity.UnityHub.png
