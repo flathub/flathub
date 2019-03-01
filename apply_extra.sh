6b330cf6ddc174169127de49ba6734dae5081202
Ryan Gonzalez <rymg19@gmail.com>
2019-03-01 15:02:52 -0600

    Updates + use unappimage to extract

--- apply_extra.sh
+++ apply_extra.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 
-chmod +x *.AppImage
-./UnityHubSetup.AppImage --appimage-extract
+unappimage *.AppImage
 mv squashfs-root/* .
-rmdir squashfs-root
+rm -rf squashfs-root *.AppImage
 # asar is a binary archive format, so the replacement string *must* be the same length
 # as the original.
 sed -i -f - resources/app.asar <<'EOF'
 s|${os\.homedir()}/\.local/share|${process.env.UNITY_DATADIR}|g;
 s|path\.parse(folder)\.root|path.parse(folder).dir |g;
+s/data => data.project_versions/dt => dt.project_versions||[]/g;
 EOF
 rm AppRun
 mkdir -p export/share/applications
