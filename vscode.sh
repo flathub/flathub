09c7ea286bfaa74d7c50fae92ec875a69e1f4486
Ryan Gonzalez <rymg19@gmail.com>
2019-03-01 15:03:55 -0600

    Have vscode-editor automatically find the correct one to use

--- vscode.sh
+++ vscode.sh
@@ -2,26 +2,12 @@
 
 flatpak='flatpak-spawn --host flatpak'
 
-vscode_title='Visual Studio Code'
-vscode_oss_title='Visual Studio Code - OSS'
+for ref in com.visualstudio.code.oss com.visualstudio.code; do
+  if $flatpak info -m $ref >/dev/null 2>&1; then
+    exec $flatpak run $ref "$@"
+  fi
+done
 
-if [[ `basename $0` == vscode-oss-editor ]]; then
-  ref=com.visualstudio.code.oss
-  title=$vscode_oss_title
-  other_title=$vscode_title
-  other_basename=vscode
-else
-  ref=com.visualstudio.code
-  title=$vscode_title
-  other_title=$vscode_oss_title
-  other_basename=vscode-oss
-fi
-
-if $flatpak info -m $ref >/dev/null 2>&1; then
-  exec $flatpak run $ref "$@"
-else
-  zenity --error --no-wrap --title="$title is not installed" \
-  --text="The $title Flatpak has not been installed, please install it from Flathub.\n\
-If you meant to use $other_title, set the editor to /app/bin/$other_basename-editor instead."
-fi
-
+zenity --error --no-wrap --title="Visual Studio Code is not installed" \
+  --text="Visual Studio Code is required to edit scripts with the Unity Hub Flatpak, please \
+install it from Flathub."
