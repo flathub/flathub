9da954960d24c1b7a4a6059ef734cf74fed7c785
eszlari <peter.eszlari@gmail.com>
2019-04-02 08:27:36 +0200

    Don't enable all SDK extensions automatically

--- code.sh
+++ code.sh
@@ -9,9 +9,5 @@ if [ ! -f ${FIRST_RUN} ]; then
   touch ${FIRST_RUN}
 fi
 
-for D in /usr/lib/sdk/*; do
-  [ -f "${D}/enable.sh" ] && . "${D}/enable.sh"
-done
-
 exec env PATH="${PATH}:${XDG_DATA_HOME}/node_modules/bin" \
   /app/extra/vscode/bin/code --extensions-dir=${XDG_DATA_HOME}/vscode/extensions "$@" ${WARNING_FILE}
