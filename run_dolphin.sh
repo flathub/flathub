60c4347ee5630e2f80562ee612b96b18af14d172
Jan Grulich <jgrulich@redhat.com>
2019-04-08 16:08:47 +0200

    Initial version of org.kde.dolphin

---
+++ run_dolphin.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ ! -f "$XDG_CONFIG_HOME/first_start" ]; then
+    echo "First start: running kbuildsycoca5"
+    touch $XDG_CONFIG_HOME/first_start
+    # Dolphin requires that ksycoca cache exists, but cannot run kbuildsycoca5
+    # automatically (because KDED lives outside of the sandbox).
+    # As a workaround we force-run it ourselves. It's really only needed once.
+    kbuildsycoca5
+fi
+
+dolphin "$@"
