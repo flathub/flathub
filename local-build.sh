e27dc730edf9124841e881a01f4bb51914e121aa
Bastien Nocera <hadess@hadess.net>
2019-09-13 12:25:33 +0200

    Build locally in the test branch

--- local-build.sh
+++ local-build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+BRANCH=test
+
 rm -f org.gnome.Totem.flatpak
 rm -rf _build ; mkdir _build
 rm -rf _repo ; mkdir _repo
 
-flatpak-builder --ccache --force-clean _build org.gnome.Totem.json --repo=_repo
+flatpak-builder --ccache --force-clean --default-branch=$BRANCH _build org.gnome.Totem.json --repo=_repo
