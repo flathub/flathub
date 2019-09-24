aa88a9dccb4737dffca3bad977b98b8a99cb89dc
Eonfge <contact@kevindegeling.nl>
2019-09-24 16:15:44 +0200

    Removed quick local run file

--- single-click-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-flatpak-builder build-dir/ io.github.ezQuake.yaml --force-clean
-flatpak-builder --repo=repo --force-clean build-dir io.github.ezQuake.yaml
-flatpak --user remote-add --no-gpg-verify --if-not-exists tutorial-repo repo
-flatpak install io.github.ezQuake -y
-flatpak update -y
