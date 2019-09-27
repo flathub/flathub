3a7f048bb7ad0dd8af83981216ffb7bf65dda093
Eonfge <contact@kevindegeling.nl>
2019-09-27 10:27:38 +0200

    Updated metadata

--- single-click-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-flatpak-builder --repo=testing-repo --arch=i386 --force-clean build-dir com.etlegacy.ETL.yaml
-flatpak --user remote-add --if-not-exists --no-gpg-verify etl-testing-repo testing-repo
-flatpak --user install etl-testing-repo com.etlegacy.ETL -y
-flatpak update -y
