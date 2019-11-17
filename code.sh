f8303d5a60d11093253c433b26e12c27b5df6da5
Rebecca Wallander <FakeShemp@users.noreply.github.com>
2019-11-17 15:22:42 +0100

    Enable Mono 5 SDK

--- code.sh
+++ code.sh
@@ -25,5 +25,9 @@ if [[ -d /usr/lib/sdk/openjdk8 ]]; then
   . /usr/lib/sdk/openjdk8/enable.sh
 fi
 
+if [[ -d /usr/lib/sdk/mono5 ]]; then
+  . /usr/lib/sdk/mono5/enable.sh
+fi
+
 exec env PATH="${PATH}:${XDG_DATA_HOME}/node_modules/bin" \
   /app/extra/vscode/bin/code --extensions-dir=${XDG_DATA_HOME}/vscode/extensions "$@" ${WARNING_FILE}
