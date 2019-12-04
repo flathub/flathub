d0f17bd46caadec5ab83ff73b37b07e777124b29
Peter Eszlari <peter.eszlari@gmail.com>
2019-12-04 13:11:14 +0100

    Use environment variable to enable SDKs

--- code.sh
+++ code.sh
@@ -9,25 +9,12 @@ if [ ! -f ${FIRST_RUN} ]; then
   touch ${FIRST_RUN}
 fi
 
-if [[ -d /usr/lib/sdk/dotnet ]]; then
-  . /usr/lib/sdk/dotnet/enable.sh
-fi
-
-if [[ -d /usr/lib/sdk/node10 ]]; then
-  . /usr/lib/sdk/node10/enable.sh
-fi
-
-if [[ -d /usr/lib/sdk/php73 ]]; then
-  . /usr/lib/sdk/php73/enable.sh
-fi
-
-if [[ -d /usr/lib/sdk/openjdk8 ]]; then
-  . /usr/lib/sdk/openjdk8/enable.sh
-fi
-
-if [[ -d /usr/lib/sdk/mono5 ]]; then
-  . /usr/lib/sdk/mono5/enable.sh
-fi
+IFS=',' read -ra SDK <<< "$FLATPAK_ENABLE_SDK"
+for i in "${SDK[@]}"; do
+  if [[ -d /usr/lib/sdk/$i ]]; then
+    . /usr/lib/sdk/$i/enable.sh
+  fi
+done
 
 exec env PATH="${PATH}:${XDG_DATA_HOME}/node_modules/bin" \
   /app/extra/vscode/bin/code --extensions-dir=${XDG_DATA_HOME}/vscode/extensions "$@" ${WARNING_FILE}
