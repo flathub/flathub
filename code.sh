449e76e9c140852bcb266a50c0555ecdb1f119a3
eszlari <peter.eszlari@gmail.com>
2020-03-03 20:28:54 +0100

    Update code.sh
    Co-Authored-By: Filippe LeMarchand <gasinvein@gmail.com>

--- code.sh
+++ code.sh
@@ -12,7 +12,11 @@ fi
 IFS=',' read -ra SDK <<< "$FLATPAK_ENABLE_SDK"
 for i in "${SDK[@]}"; do
   if [[ -d /usr/lib/sdk/$i ]]; then
-    . /usr/lib/sdk/$i/enable.sh
+    if [[ -f /usr/lib/sdk/$i/enable.sh ]]; then
+      . /usr/lib/sdk/$i/enable.sh
+    else
+      export PATH=$PATH:/usr/lib/sdk/$i/bin
+    fi
   fi
 done
 
