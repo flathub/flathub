397fdd79e0e0a5466b60839ed5110651a8408e98
x80486 <masked.github@mailnull.com>
2020-06-22 13:52:53 -0400

    cryptomator flatpak

---
+++ cryptomator.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -o errexit
+
+PATH="${PATH}:/app/bin:/app/jre/bin:/usr/bin"
+export PATH
+TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
+export TMPDIR
+
+java --class-path "/app/Cryptomator/*" \
+  -Dcryptomator.ipcPortPath="${XDG_CONFIG_HOME}"/Cryptomator/ipcPort.bin \
+  -Dcryptomator.logDir="${XDG_DATA_HOME}"/Cryptomator/logs/ \
+  -Dcryptomator.mountPointsDir="${XDG_DATA_HOME}"/Cryptomator/mnt/ \
+  -Dcryptomator.settingsPath="${XDG_CONFIG_HOME}"/Cryptomator/settings.json \
+  -Xmx512m \
+  -Xss20m \
+  org.cryptomator.launcher.Cryptomator
