47dae51ad5b084f37571fd0702a57f7ef0ef9a55
Nicola Corna <nicola@corna.info>
2020-07-07 09:14:57 +0200

    Tested and working

--- installer.sh
+++ installer.sh
@@ -12,33 +12,31 @@ sed -i "s|^CreateDesktopShortcuts=.*|CreateDesktopShortcuts=1|" "$FLATPAK_BUILDE
 # Run the installer
 "$FLATPAK_BUILDER_BUILDDIR/installer/xsetup" --batch Install --agree XilinxEULA,3rdPartyEULA,WebTalkTerms --config "$FLATPAK_BUILDER_BUILDDIR/xilinx_unified_installer.conf"
 
-# Link the binaries
+# Link the binaries (can't use symlinks for Vivado and Vitis, as they require $0 to be set to the real binary path)
 mkdir -p /app/bin
-ln -s "$PREFIX/Vivado/$VERSION/bin/vivado" /app/bin/com.xilinx.Vivado
-ln -s "$PREFIX/DocNav/docnav" /app/bin/com.xilinx.Vivado.Docnav
-ln -s "$PREFIX/Vitis/$VERSION/bin/vitis" /app/bin/com.xilinx.Vivado.Vitis
-ln -s "$PREFIX/Vitis/$VERSION/bin/vitis_hls" /app/bin/com.xilinx.Vivado.HLS
+echo -e '#!'"/bin/sh\n\nexport LD_LIBRARY_PATH=/app/lib\nexec $PREFIX/Vivado/$VERSION/bin/vivado" > /app/bin/com.xilinx.Vivado
+echo -e '#!'"/bin/sh\n\nexport LD_LIBRARY_PATH=/app/lib\nexec $PREFIX/Vitis/$VERSION/bin/vitis" > /app/bin/com.xilinx.Vivado.Vitis
+echo -e '#!'"/bin/sh\n\nexport LD_LIBRARY_PATH=/app/lib\nexec $PREFIX/Vitis/$VERSION/bin/vitis_hls" > /app/bin/com.xilinx.Vivado.HLS
+chmod 755 /app/bin/com.xilinx.Vivado
+chmod 755 /app/bin/com.xilinx.Vivado.Vitis
+chmod 755 /app/bin/com.xilinx.Vivado.HLS
+ln -s "$PREFIX/DocNav/docnav" /app/bin/com.xilinx.Vivado.DocNav
 
 # Install the icons
 install -Dm644 "$PREFIX/Vivado/$VERSION/doc/images/vivado_logo.png" /app/share/icons/hicolor/64x64/apps/com.xilinx.Vivado.png
-install -Dm644 "$PREFIX/DocNav/resources/doc_nav_application_48.png" /app/share/icons/hicolor/48x48/apps/com.xilinx.Vivado.Docnav.png
+install -Dm644 "$PREFIX/DocNav/resources/doc_nav_application_48.png" /app/share/icons/hicolor/48x48/apps/com.xilinx.Vivado.DocNav.png
 icotool -x -w 256 -o "$FLATPAK_BUILDER_BUILDDIR/ide_icon.png" "$PREFIX/Vitis/$VERSION/doc/images/ide_icon.ico"
 install -Dm644 "$FLATPAK_BUILDER_BUILDDIR/ide_icon.png" /app/share/icons/hicolor/256x256/apps/com.xilinx.Vivado.Vitis.png
 install -Dm644 "$FLATPAK_BUILDER_BUILDDIR/ide_icon.png" /app/share/icons/hicolor/256x256/apps/com.xilinx.Vivado.HLS.png
 
 # Install the desktop files
 install -Dm644 "$HOME/Desktop/Vivado $VERSION.desktop" /app/share/applications/com.xilinx.Vivado.desktop
-install -Dm644 "$HOME/Desktop/Documentation Navigator.desktop" /app/share/applications/com.xilinx.Vivado.Docnav.desktop
+install -Dm644 "$HOME/Desktop/Documentation Navigator.desktop" /app/share/applications/com.xilinx.Vivado.DocNav.desktop
 install -Dm644 "$HOME/Desktop/Xilinx Vitis $VERSION.desktop" /app/share/applications/com.xilinx.Vivado.Vitis.desktop
 install -Dm644 "$HOME/Desktop/Vitis HLS $VERSION.desktop" /app/share/applications/com.xilinx.Vivado.HLS.desktop
 
 # Adjust "Exec" and "Icon" of the installed desktop files
 desktop-file-edit --set-key=Exec --set-value=com.xilinx.Vivado --set-icon=com.xilinx.Vivado /app/share/applications/com.xilinx.Vivado.desktop
-desktop-file-edit --set-key=Exec --set-value=com.xilinx.Vivado.Docnav --set-icon=com.xilinx.Vivado.Docnav /app/share/applications/com.xilinx.Vivado.Docnav.desktop
+desktop-file-edit --set-key=Exec --set-value=com.xilinx.Vivado.DocNav --set-icon=com.xilinx.Vivado.DocNav /app/share/applications/com.xilinx.Vivado.DocNav.desktop
 desktop-file-edit --set-key=Exec --set-value=com.xilinx.Vivado.Vitis --set-icon=com.xilinx.Vivado.Vitis /app/share/applications/com.xilinx.Vivado.Vitis.desktop
 desktop-file-edit --set-key=Exec --set-value=com.xilinx.Vivado.HLS --set-icon=com.xilinx.Vivado.HLS /app/share/applications/com.xilinx.Vivado.HLS.desktop
-
-# Vivado requires libtinfo5
-mkdir -p /app/lib
-ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6.1 /app/lib/libtinfo.so.5
-
