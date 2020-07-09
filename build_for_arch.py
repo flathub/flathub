32c8ff7d83a60581bb994bd1f9b6c10ed53b9b8c
advaithm <advaith.mahdukar@gmail.com>
2020-07-09 19:27:09 +0530

    add stdio output

--- build_for_arch.py
+++ build_for_arch.py
@@ -2,8 +2,11 @@ import platform
 import os
 arch = platform.architecture()
 if(arch[0]=='64bit'):
+    print("extracting nuclear-fca030.tar.gz")
     os.system('tar --extract -f nuclear-fca030.tar.gz')
+    print("creating required dirs")
     os.system('mkdir -p /app/main /app/bin /app/share/metainfo/  /app/share/applications/')
+    print("copying binares to main")
     os.system('cp -r  nuclear-fca030/* /app/main/')
 else:
     print('Unsupported arch')
