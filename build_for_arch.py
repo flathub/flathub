6e0ac0481ab04c827df3a67decbfb3e3310fb534
advaithm <advaith.mahdukar@gmail.com>
2020-07-14 08:38:34 +0530

    updated to remove uneeded mkdirs.

--- build_for_arch.py
+++ build_for_arch.py
@@ -5,7 +5,7 @@ if(arch[0]=='64bit'):
     print("extracting nuclear-fca030.tar.gz")
     os.system('tar --extract -f nuclear-fca030.tar.gz')
     print("creating required dirs")
-    os.system('mkdir -p /app/main /app/bin /app/share/metainfo/  /app/share/applications/')
+    os.system('mkdir -p /app/main ')
     print("copying binares to main")
     os.system('cp -r  nuclear-fca030/* /app/main/')
 else:
