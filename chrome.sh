7aeccece837f6a838f9eba6001cda8a8ef06db16
Ryan Gonzalez <rymg19@gmail.com>
2020-07-19 02:49:51 -0500

    Pull in the host's policies

--- chrome.sh
+++ chrome.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/bash
 
-policy_dir=/etc/opt/chrome/policies/managed
-mkdir -p "$policy_dir"
-ln -sf /app/share/flatpak-chrome/flatpak_policy.json "$policy_dir"
+policy_root=/etc/opt/chrome/policies
+
+for policy_type in managed recommended; do
+  policy_dir="$policy_root/$policy_type"
+  mkdir -p "$policy_dir"
+
+  if [[ "$policy_type" == 'managed' ]]; then
+    ln -sf /app/share/flatpak-chrome/flatpak_policy.json "$policy_dir"
+  fi
+
+  if [[ -d "/run/host/$policy_root/$policy_type" ]]; then
+    find "/run/host/$policy_root/$policy_type" -name '*.json' \
+      -exec ln -s '{}' "$policy_root/$policy_type" \;
+  fi
+done
 
 run_stamp="$XDG_DATA_HOME/flatpak-chrome-run-stamp"
 mimic_stamp="$XDG_DATA_HOME/flatpak-chrome-mimic-stamp"
