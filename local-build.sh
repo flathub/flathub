43674a38ecf66ab80ca4ab3c979662bdd693bde8
Bastien Nocera <hadess@hadess.net>
2020-09-01 14:03:54 +0200

    Remove bundle creation (too big)

--- local-build.sh
+++ local-build.sh
@@ -5,5 +5,3 @@ rm -rf _build ; mkdir _build
 rm -rf _repo ; mkdir _repo
 
 flatpak-builder --ccache --force-clean _build org.ghidra_sre.Desktop.json --repo=_repo
-flatpak build-bundle _repo org.ghidra_sre.Desktop.flatpak org.ghidra_sre.Desktop stable
-
