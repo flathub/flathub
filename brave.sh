ea603cb652577bc847347121acde8edbf108d7db
user <user@debian>
2020-09-24 21:35:55 +0300

    fix license and descriptions

--- brave.sh
+++ brave.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 export TMPDIR="$XDG_RUNTIME_DIR/app/$FLATPAK_ID"
-exec zypak-wrapper /app/extra/brave "$@"
+which zypak-wrapper    2>/dev/null >/dev/null && wrapper="zypak-wrapper"
+which zypak-wrapper.sh 2>/dev/null >/dev/null && wrapper="zypak-wrapper.sh"
+exec $wrapper /app/extra/brave "$@"
