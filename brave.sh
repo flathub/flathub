127c68d68dda62a1c977e27a748f1b109d46fad8
user <user@debian>
2020-09-24 21:37:11 +0300

    fix zypak-warpper

--- brave.sh
+++ brave.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/bash
 export TMPDIR="$XDG_RUNTIME_DIR/app/$FLATPAK_ID"
-which zypak-wrapper    2>/dev/null >/dev/null && wrapper="zypak-wrapper"
-which zypak-wrapper.sh 2>/dev/null >/dev/null && wrapper="zypak-wrapper.sh"
-exec $wrapper /app/extra/brave "$@"
+exec zypak-wrapper /app/extra/brave "$@"
