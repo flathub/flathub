70eae80a64b64302b933c718c3a999ba82dafdf3
user <user@debian>
2020-09-24 16:32:26 +0300

    rename everything from Chrome to Brave

--- apply_extra.sh
+++ apply_extra.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
-ar x chrome.deb data.tar.xz
-rm chrome.deb
+ar x brave.deb data.tar.xz
+rm brave.deb
 tar -xvf data.tar.xz --strip-components=4 ./opt/brave.com/brave
 rm data.tar.xz
 cp /app/bin/stub_sandbox chrome-sandbox
