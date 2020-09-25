3e88f42aaf20e37a0cfb9e8b1d21ea783b5c4bd9
user <user@debian>
2020-09-25 11:29:39 +0300

    nacl doesn't even exist so nothing to remove

--- apply_extra.sh
+++ apply_extra.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/sh
 
 main() {
-ar x brave.deb data.tar.xz
-rm -f brave.deb
-tar -xvf data.tar.xz --strip-components=4 ./opt/brave.com/brave
-rm -f data.tar.xz nacl*
+ar x brave.deb data.tar.xz; rm -f brave.deb
+tar -xvf data.tar.xz --strip-components=4 ./opt/brave.com/brave; rm -f data.tar.xz
 }
 main 2>/dev/null >/dev/null
