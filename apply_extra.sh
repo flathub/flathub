3e25ee2ad4a80d41aa2958ec61390ffb69942f56
user <user@debian>
2020-09-25 11:25:16 +0300

    disable zypak for good, get rid of sandbox warning, and more
    this fixes all the issues caused by using zypak

--- apply_extra.sh
+++ apply_extra.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/sh
+
+main() {
 ar x brave.deb data.tar.xz
-rm brave.deb
+rm -f brave.deb
 tar -xvf data.tar.xz --strip-components=4 ./opt/brave.com/brave
-rm data.tar.xz
-cp /app/bin/stub_sandbox chrome-sandbox
+rm -f data.tar.xz nacl*
+}
+main 2>/dev/null >/dev/null
--- brave.sh
+++ brave.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/bash
 export TMPDIR="$XDG_RUNTIME_DIR/app/$FLATPAK_ID"
-exec zypak-wrapper.sh /app/extra/brave "$@"
-#exec /app/extra/brave --no-sandbox "$@"
+exec /app/extra/brave --test-type --no-sandbox "$@"
