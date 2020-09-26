04c22a15666c60b34606b72d2ffa4262b5566b94
user <user@debian>
2020-09-26 15:30:31 +0300

    delete mimic_stamp when we use user namespaces
    that way, the user will be alerted if he was using user namespaces but was downgraded to suid

--- brave.sh
+++ brave.sh
@@ -5,6 +5,8 @@ mimic_stamp="$XDG_DATA_HOME/flatpak-brave-mimic-stamp"
 if [ ! -f "$mimic_stamp" ] && ! zypak-helper spawn-strategy-test; then
 	zenity --info --title='Brave Flatpak' --no-wrap --text="$(< /app/share/flatpak-brave/mimic_warning.txt)"
 	> "$mimic_stamp"
+elif [ -f "$mimic_stamp" ]; then
+	rm -f "$mimic_stamp"
 fi
 
 export TMPDIR="$XDG_RUNTIME_DIR/app/$FLATPAK_ID"
