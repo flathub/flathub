0667a49415c652abcb5922cb4d15ee4917f35afc
user <user@debian>
2020-09-27 18:55:18 +0300

    initial attempt

---
+++ chromium.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+
+# Show warning for people that don't have expose-pids support every time
+if ! zypak-helper spawn-strategy-test; then
+	set /app/share/flatpak-chromium/mimic_warning.html chrome://welcome "$@"
+fi
+
+export TMPDIR="$XDG_RUNTIME_DIR/app/$FLATPAK_ID"
+exec zypak-wrapper.sh /app/extra/chromium-browser "$@"
