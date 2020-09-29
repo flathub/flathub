8e91d3d056056166c7e3ed337a9d85643c24521a
user <user@debian>
2020-09-29 09:10:43 +0300

    fix launch

--- chromium.sh
+++ chromium.sh
@@ -6,4 +6,4 @@ if ! zypak-helper spawn-strategy-test; then
 fi
 
 export TMPDIR="$XDG_RUNTIME_DIR/app/$FLATPAK_ID"
-exec zypak-wrapper.sh /app/share/chromium-browser/chrome-browser "$@"
+exec zypak-wrapper.sh /app/share/chromium-browser/chrome "$@"
