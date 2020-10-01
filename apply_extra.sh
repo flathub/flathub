2565a9d9c2a8627e089b3a7b188dd62782ce39bd
analotia <analotia@i2pmail.org>
2020-10-01 20:59:40 +0300

    revert for now

--- apply_extra.sh
+++ apply_extra.sh
@@ -3,9 +3,9 @@
 main() {
 ar x brave.deb data.tar.xz; rm -f brave.deb
 tar -xvf data.tar.xz --strip-components=4 ./opt/brave.com/brave
-tar -xvf data.tar.xz --strip-components=4 ./usr/share/applications/brave-browser.desktop
-sed -e 's/Exec=\/usr\/bin\/brave-browser-stable/Exec=\/app\/bin\/brave/g' -i brave-browser.desktop
-sed -e 's/Icon=brave-browser/Icon=com.brave.Browser/g' -i brave-browser.desktop
-mv brave-browser.desktop /app/share/applications/com.brave.Browser.desktop
+#tar -xvf data.tar.xz --strip-components=4 ./usr/share/applications/brave-browser.desktop
+rm -f data.tar.xz
+#sed -e 's/Exec=\/usr\/bin\/brave-browser-stable/Exec=\/app\/bin\/brave/g' -i brave-browser.desktop
+#sed -e 's/Icon=brave-browser/Icon=com.brave.Browser/g' -i brave-browser.desktop
 }
 main 2>/dev/null >/dev/null
