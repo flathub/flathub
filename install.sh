698bed7f981978f67f988a1ee8f30a3e383e579a
Ahmad <34627272+AhmadHrb@users.noreply.github.com>
2020-12-11 15:14:56 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-build-commands:
-  # Install npm dependencies
-  - npm install --prefix=main --offline --cache=/run/build/pocket-browser/npm-cache/
-  # Bundle app and dependencies
-  - mkdir -p /app/main /app/bin
-  - cp -ra main/* /app/main/
-  # Install app wrapper
-  - install install.sh /app/bin/
