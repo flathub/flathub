b59a33c52d2f8772858d41b040b18a637aeafbf6
out <outline941@live.com>
2021-01-17 09:56:04 +0800

    First commit

---
+++ gen-sources.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [ ! -d joplin ];then
+	url=`grep -oE "https://[^\"]*" ./org.joplinapp.Joplin.yml | grep joplin`
+	wget $url -O joplin.tar && tar -xf joplin.tar
+	mv joplin-* joplin
+fi
+
+if [ ! -d joplin/git ];then
+    mkdir joplin/git
+	pushd joplin/git
+	git clone https://github.com/laurent22/node-emoji.git
+	git clone https://github.com/laurent22/uslug.git
+	cd uslug && git checkout emoji-support
+	popd
+fi
+
+nodegen=./flatpak-builder-tools/node/flatpak-node-generator.py
+packages="app-cli app-desktop fork-htmlparser2 renderer turndown fork-sax lib tools turndown-plugin-gfm"
+src=joplin
+
+for pack in $packages;do
+	echo "add $pack"
+	pattern="$pattern -R $src/packages/$pack/package-lock.json"
+done
+pattern="$pattern -R joplin/git/uslug/package-lock.json"
+pattern="$pattern -R joplin/git/node-emoji/package-lock.json"
+$nodegen --xdg-layout -r -R joplin/package-lock.json $pattern npm joplin/package-lock.json
