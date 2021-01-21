51b6430597d2a0f411544dbe7b056e4e954d7156
Bastien Nocera <hadess@hadess.net>
2021-01-21 12:18:01 +0100

    Use 32-bit compat extensions

--- local-build.sh
+++ local-build.sh
@@ -4,5 +4,5 @@ rm -f com.carpeludum.KegaFusion.flatpak
 rm -rf _build ; mkdir _build
 rm -rf _repo ; mkdir _repo
 
-flatpak-builder --arch i386 --ccache --force-clean _build com.carpeludum.KegaFusion.json --repo=_repo
-flatpak build-bundle --arch i386 _repo com.carpeludum.KegaFusion.flatpak com.carpeludum.KegaFusion master
+flatpak-builder --ccache --force-clean _build com.carpeludum.KegaFusion.yml --repo=_repo
+flatpak build-bundle _repo com.carpeludum.KegaFusion.flatpak com.carpeludum.KegaFusion master
