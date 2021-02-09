801323d7ef0bd04212c451625ce04d313647a31f
Bastien Nocera <hadess@hadess.net>
2021-02-09 09:11:49 +0100

    Rename to org.ghidra_sre.Ghidra

--- local-build.sh
+++ local-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-rm -f org.ghidra_sre.Desktop.flatpak
+rm -f org.ghidra_sre.Ghidra.flatpak
 rm -rf _build ; mkdir _build
 rm -rf _repo ; mkdir _repo
 
-flatpak-builder --ccache --force-clean _build org.ghidra_sre.Desktop.json --repo=_repo
+flatpak-builder --ccache --force-clean _build org.ghidra_sre.Ghidra.json --repo=_repo
