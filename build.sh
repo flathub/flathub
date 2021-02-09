988ab87706b10f6445a7e898a980602c8f495d9b
Simon McVittie <smcv@collabora.com>
2021-02-09 20:09:54 +0000

    Replace convenience Makefile with a convenience script
    Signed-off-by: Simon McVittie <smcv@collabora.com>

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+mkdir -p _source
+flatpak-builder \
+    --force-clean \
+    --repo=_build/repo \
+    --extra-sources=_source \
+    _build \
+    com.valvesoftware.steamlink.yml
+
+flatpak \
+    build-bundle \
+    _build/repo \
+    _build/steamlink.flatpak \
+    com.valvesoftware.steamlink
+
+echo "Install with: flatpak install --bundle /path/to/steamlink.flatpak"
