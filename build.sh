f114726befa9d9f13d67443b50f3b3a7b85421db
Simon McVittie <smcv@collabora.com>
2021-02-11 17:37:16 +0000

    Switch app ID to preferred case combination
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- build.sh
+++ build.sh
@@ -6,12 +6,12 @@ flatpak-builder \
     --repo=_build/repo \
     --extra-sources=_source \
     _build \
-    com.valvesoftware.steamlink.yml
+    com.valvesoftware.SteamLink.yml
 
 flatpak \
     build-bundle \
     _build/repo \
     _build/steamlink.flatpak \
-    com.valvesoftware.steamlink
+    com.valvesoftware.SteamLink
 
 echo "Install with: flatpak install --bundle /path/to/steamlink.flatpak"
