77ce1974faf2d36de2acd73839a945a36fe2a26b
Bastien Nocera <hadess@hadess.net>
2021-09-30 16:58:42 +0200

    Update for GStreamer repo reorg
    Take the opportunity of GStreamer mono repository reorganisation:
    https://gstreamer.freedesktop.org/documentation/frequently-asked-questions/mono-repository.html
    to build the whole of the GStreamer stack ourselves.
    
    GStreamer usually releases its new point releases in October, the
    Freedesktop Flatpak runtime in August. This means that our application
    would get updates nearly a year after the fact, a pace somewhat too slow
    for an application that relies so much on upstream bug fixes to work
    properly and handle "edge cases" media files.

---
+++ codecs/move-gst-plugins.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+mkdir -p /app/lib/codecs/lib/gstreamer-1.0
+export GST_PLUGIN_SYSTEM_PATH=/app/lib/gstreamer-1.0
+
+for i in /app/lib/gstreamer-1.0/*.so; do
+  SUITE=`gst-inspect-1.0 $i | grep "Source module" | sed 's,Source module,,' | tr -d [:blank:]`
+  if [ x$SUITE == x'gst-plugins-bad' ] ||
+     [ x$SUITE == x'gst-plugins-ugly' ] ||
+     [ x$SUITE == x'gst-libav' ]; then
+    mv $i /app/lib/codecs/lib/gstreamer-1.0
+  fi
+done
+
