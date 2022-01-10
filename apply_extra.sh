21d0a153219912d71c1a097b81f06a3fcfbdbeaa
doomsdayrs <doomsdayrs@gmail.com>
2022-01-10 00:33:16 -0500

    Fixed icon directory

--- apply_extra.sh
+++ apply_extra.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 SRC="/app/extra/wt.tar.gz"
-XDG_DATA_HOME="/app/extra/export/tmp/"
+XDG_DATA_HOME="/app/extra/tmp/"
 
 # Setup
 mkdir -p $XDG_DATA_HOME
@@ -10,8 +10,8 @@ mkdir -p $XDG_DATA_HOME
 tar -xv --gzip -f $SRC -C $XDG_DATA_HOME
 
 # Export icon
-mkdir -p "/app/extra/export/share/icons/128x128/apps/"
-mv -v "$XDG_DATA_HOME/WarThunder/launcher.ico" "/app/extra/export/share/icons/128x128/apps/net.gaijin.WarThunder.png"
+mkdir -p "/app/extra/export/share/icons/hicolor/128x128/apps/"
+mv -v "$XDG_DATA_HOME/WarThunder/launcher.ico" "/app/extra/export/share/icons/hicolor/128x128/apps/net.gaijin.WarThunder.png"
 
 # Clean up
 rm -rf $XDG_DATA_HOME
