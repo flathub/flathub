786ad3230135356e2e157d2dbd3ec8840bbda8f5
doomsdayrs <doomsdayrs@gmail.com>
2022-01-10 00:12:40 -0500

    Correct apply_extra.sh & run.sh

--- apply_extra.sh
+++ apply_extra.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -e
 
 SRC="/app/extra/wt.tar.gz"
+XDG_DATA_HOME="/app/extra/export/tmp/"
 
-# Unload launcher into data directory
-echo $SRC
-echo $XDG_DATA_HOME
+# Setup
+mkdir -p $XDG_DATA_HOME
 
+# Extract
 tar -xv --gzip -f $SRC -C $XDG_DATA_HOME
 
 # Export icon
-mkdir "/app/extra/export/share/icons/128x128/apps/"
+mkdir -p "/app/extra/export/share/icons/128x128/apps/"
 mv -v "$XDG_DATA_HOME/WarThunder/launcher.ico" "/app/extra/export/share/icons/128x128/apps/net.gaijin.WarThunder.png"
+
+# Clean up
+rm -rf $XDG_DATA_HOME
--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
 
+SRC="/app/extra/wt.tar.gz"
+
+# Extract
+tar -xv --gzip -f $SRC -C $XDG_DATA_HOME
+
 # Execute launcher
 exec "${XDG_DATA_HOME}/WarThunder/launcher"
