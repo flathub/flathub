12814df721317bf5341b03c160f2e4529f5ab53f
Doomsdayrs <38189170+Doomsdayrs@users.noreply.github.com>
2022-01-10 02:29:08 -0500

    Update run.sh
    Performance improvements :D
    
    Co-authored-by: TheEvilSkeleton <theevilskeleton@riseup.net>

--- run.sh
+++ run.sh
@@ -3,7 +3,10 @@
 SRC="/app/extra/wt.tar.gz"
 
 # Extract
-tar -xv --gzip -f $SRC -C $XDG_DATA_HOME
+if [[ ! -f "$XDG_DATA_HOME/sha_check" ]] || [[ "$(sha256sum /app/extra/wt.tar.gz | cut -d' ' -f1)" != "$(cat "$XDG_DATA_HOME"/sha_check 2>/dev/null)" ]]; then
+	sha256sum /app/extra/wt.tar.gz | cut -d' ' -f1 > "$XDG_DATA_HOME"/sha_check
+	tar -xv --gzip -f "$SRC" -C "$XDG_DATA_HOME"
+fi
 
 # Execute launcher
 exec "${XDG_DATA_HOME}/WarThunder/launcher"
