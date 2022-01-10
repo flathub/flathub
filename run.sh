44c280d828f8ba726ceff869d0df067be13d2cab
doomsdayrs <doomsdayrs@gmail.com>
2022-01-10 15:29:49 -0500

    Use version file for tar extraction

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -e
 
 SRC="/app/extra/wt.tar.gz"
+FLAT_VERSION="/app/bin/version"
+DATA_VERSION="${XDG_DATA_HOME}/version"
 
 # Extract
-if [[ ! -f "$XDG_DATA_HOME/sha_check" ]] || [[ "$(sha256sum /app/extra/wt.tar.gz | cut -d' ' -f1)" != "$(cat "$XDG_DATA_HOME"/sha_check 2>/dev/null)" ]]; then
-	sha256sum /app/extra/wt.tar.gz | cut -d' ' -f1 > "$XDG_DATA_HOME"/sha_check
+if [[ ! -f "$DATA_VERSION" ]] || [[ `cmp --silent "$FLAT_VERSION" "$DATA_VERSION"; echo $?` -ne 0 ]] ; then
+	cp -f "$FLAT_VERSION" "$DATA_VERSION"
 	tar -xv --gzip -f "$SRC" -C "$XDG_DATA_HOME"
 fi
 
