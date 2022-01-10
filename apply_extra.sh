3f07ac379ce48831d5b0a3d1369c86a9c98a5074
doomsdayrs <doomsdayrs@gmail.com>
2022-01-10 02:28:36 -0500

    Fixed metainfo & added direct icon storage

--- apply_extra.sh
+++ apply_extra.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -e
 
-SRC="/app/extra/wt.tar.gz"
-XDG_DATA_HOME="/app/extra/tmp/"
+#SRC="/app/extra/wt.tar.gz"
+#XDG_DATA_HOME="/app/extra/tmp/"
 
 # Setup
-mkdir -p $XDG_DATA_HOME
+#mkdir -p $XDG_DATA_HOME
 
 # Extract
-tar -xv --gzip -f $SRC -C $XDG_DATA_HOME
+#tar -xv --gzip -f $SRC -C $XDG_DATA_HOME
 
 # Export icon
-mkdir -p "/app/extra/export/share/icons/hicolor/128x128/apps/"
-mv -v "$XDG_DATA_HOME/WarThunder/launcher.ico" "/app/extra/export/share/icons/hicolor/128x128/apps/net.gaijin.WarThunder.png"
+#mkdir -p "/app/extra/export/share/icons/hicolor/128x128/apps/"
+#mv -v "$XDG_DATA_HOME/WarThunder/launcher.ico" "/app/extra/export/share/icons/hicolor/128x128/apps/net.gaijin.WarThunder.png"
 
 # Clean up
-rm -rf $XDG_DATA_HOME
+#rm -rf $XDG_DATA_HOME
