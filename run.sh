f70d9287b9aa097609f7948c40bfb19c888ab6bd
doomsdayrs <doomsdayrs@gmail.com>
2022-01-23 16:11:58 -0500

    Impl custom dir

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -e
 
 SRC="/app/extra/wt.tar.gz"
+DIR=${CUSTOM_DIR:-$XDG_DATA_HOME}
 FLAT_VERSION="/app/bin/version"
-DATA_VERSION="${XDG_DATA_HOME}/version"
+DATA_VERSION="${DIR}/WarThunder/version"
 
 # Extract
 if [[ ! -f "$DATA_VERSION" ]] || [[ `cmp --silent "$FLAT_VERSION" "$DATA_VERSION"; echo $?` -ne 0 ]] ; then
 	cp -f "$FLAT_VERSION" "$DATA_VERSION"
-	tar -xv --gzip -f "$SRC" -C "$XDG_DATA_HOME"
+	tar -xv --gzip -f "$SRC" -C "$DIR"
 fi
 
 # Execute launcher
-exec "${XDG_DATA_HOME}/WarThunder/launcher"
+exec "${DIR}/WarThunder/launcher"
