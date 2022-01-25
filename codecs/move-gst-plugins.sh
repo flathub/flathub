e1e1268df49c7b334898be764e5018c0048c8539
Bastien Nocera <hadess@hadess.net>
2022-01-25 22:41:20 +0100

    Fix libav codecs not working anymore
    Tell gstreamer where to get the newer headers and libraries for ffmpeg
    rather than relying on the SDK's default paths to include a compatible
    version.

--- codecs/move-gst-plugins.sh
+++ codecs/move-gst-plugins.sh
@@ -2,6 +2,7 @@
 
 mkdir -p /app/lib/codecs/lib/gstreamer-1.0
 export GST_PLUGIN_SYSTEM_PATH=/app/lib/gstreamer-1.0
+export LD_LIBRARY_PATH=/app/lib/codecs/lib/:/app/lib/
 
 for i in /app/lib/gstreamer-1.0/*.so; do
   SUITE=`gst-inspect-1.0 $i | grep "Source module" | sed 's,Source module,,' | tr -d [:blank:]`
