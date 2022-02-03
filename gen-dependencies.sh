b5328719a4482109b83b127d8c558ad6e6e45c05
catsout <outline941@live.com>
2022-02-03 12:22:55 +0800

    more cleanup, separate pyqt and fix permission

--- gen-dependencies.sh
+++ gen-dependencies.sh
@@ -3,3 +3,6 @@ pip_generator="$PWD/flatpak-builder-tools/pip/flatpak-pip-generator"
 cd dependencies
 ${pip_generator} -r sigil-requirements.txt --output sigil-pypi-dependencies
 ${pip_generator} -r pyqt5-requirements.txt --output pyqt5-pypi-dependencies
+
+# cleanup PyQt-builder
+sed -i -E 's|(PyQt-builder",)|\1"cleanup":["*"],|g' pyqt5-pypi-dependencies.json
