2be718d8622249589f4eb9bdeafcfa531027d70f
Ralph Plawetzki <ralph@purejava.org>
2022-02-13 13:46:23 +0100

    Finishing build steps

--- cryptomator.sh
+++ cryptomator.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
 java --class-path "/app/Cryptomator/libs/*" \
+  --module-path "/app/Cryptomator/mods" \
   -Dcryptomator.ipcPortPath="${XDG_CONFIG_HOME}"/Cryptomator/ipcPort.bin \
   -Dcryptomator.logDir="${XDG_DATA_HOME}"/Cryptomator/logs/ \
   -Dcryptomator.mountPointsDir="${XDG_DATA_HOME}"/Cryptomator/mnt/ \
   -Dcryptomator.settingsPath="${XDG_CONFIG_HOME}"/Cryptomator/settings.json \
-  -Xmx512m \
-  -Xss20m \
-  org.cryptomator.launcher.Cryptomator
+  -Dcryptomator.ipcSocketPath="${XDG_CONFIG_HOME}"/Cryptomator/ipc.socket \
+	-Xss2m \
+	-Xmx512m \
+	-m org.cryptomator.desktop/org.cryptomator.launcher.Cryptomator
