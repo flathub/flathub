0b2f0100ca4d7f274438b06ddfbe92f8790a80ad
Nikolai Wuttke <lethal_guitar128@web.de>
2022-05-24 10:03:49 +0200

    Use XDG_DATA_HOME for game files

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-RigelEngine "$@" ~/rigelengine-game-data
+GAME_PATH="$XDG_DATA_HOME/Duke2"
+
+if ls "$GAME_PATH/NUKEM2.CMP" > /dev/null 2>&1; then
+    RigelEngine "$@" "$GAME_PATH"
+else
+    zenity --error --text "Missing data - place your Duke Nukem II game data into:\n$GAME_PATH" --ok-label "Close" --width=500
+fi
