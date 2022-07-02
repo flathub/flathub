4df2102630470e1f48443cfa331dc06e69a886a5
Steam Deck User <XargonWan@gmail.com>
2022-07-02 22:42:16 +0200

    Updating flathub

--- tools/start-yuzu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-zenity --title "RetroDECK" --question --no-wrap --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" --text="Doing some changes in the emulator's configuration may create serious issues,\nplease continue only if you know what you're doing.\n\nDo you want to continue?"
-if [ $? == 0 ]
-then
-    yuzu
-fi
