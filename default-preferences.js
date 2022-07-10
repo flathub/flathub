e66ed1ea4f01d638f22d3c97b675566a7e4470fe
HZ <99131470+hustler-not-chatty@users.noreply.github.com>
2022-07-11 01:44:25 +0800

    Initial commit.

---
+++ default-preferences.js
@@ -0,0 +1,6 @@
+pref("intl.locale.requested",               "");
+pref("app.update.auto",                     false);
+pref("app.update.enabled",                  false);
+pref("app.update.autoInstallEnabled",       false);
+pref("browser.shell.checkDefaultBrowser",   false);
+
