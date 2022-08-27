573b51dba0f270b3e4dd0f5166d6a2e1859d9c62
HZ <99131470+hustler-not-chatty@users.noreply.github.com>
2022-08-28 02:20:29 +0800

    process.py: various fixes - Fix wrong insertion - Add parameters (primarily for local testing) - Now merging modules from Mozilla's BaseApp.

--- process.py
+++ process.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from datetime import datetime
 import json
-from urllib import request
+import argparse
 
 from packaging.version import parse
 from fetcher import build_language_list, build_sha256dict, current_esr_version, get_build_info
@@ -12,6 +11,23 @@ VERSIONS_API_URL = 'https://product-details.mozilla.org/1.0/firefox_versions.jso
 
 platform = 'linux'
 arch = 'x86_64'
+
+
+class MyJsonTapper:
+    json_data = {}
+
+    def __init__(self, file_path) -> None:
+        with open(file_path, 'r') as fi:
+            self.json_data = json.load(fi)
+
+    def tap(self, func, *args):
+        func(self.json_data, *args)
+        return self
+
+    def output(self, output_path):
+        with open(output_path, 'w') as fo:
+            fo.write(json.dumps(self.json_data, indent=4))
+
 
 def get_last_ver():
     last = ''
@@ -23,11 +39,13 @@ def get_last_ver():
         pass
     return last
 
+
 def set_last_ver(last):
     with open('./last', 'w') as f:
         f.write(last)
 
-def replace_vars(input_path, output_path, **vars):
+
+def varsubst(input_path, output_path, **vars):
     print(input_path, output_path, vars)
     with open(input_path, 'r') as f:
         content = f.read()
@@ -36,16 +54,9 @@ def replace_vars(input_path, output_path, **vars):
         with open(output_path, 'w') as fo:
             fo.write(content)
 
-def json_insert_modules(input_file, output_file, modules):
-    with open(input_file) as fi:
-        jsonInput = json.load(fi)
-        for lp in modules:
-            jsonInput['modules'].append(lp)
-        with open(output_file, 'w') as fo:
-            fo.write(json.dumps(jsonInput, indent=4))
-    
-def build_modules(sha256_dict, languages):
-    modules = [{
+
+def extend_firefox_source(json_object, sha256_dict, languages, version):
+    sources_to_extend = [{
         'type': 'archive',
         'url': f'{BINARY_BASE_URL}/{version}/{platform}-{arch}/en-US/firefox-{version}.tar.bz2',
         'sha256': sha256_dict[f'{platform}-{arch}/en-US/firefox-{version}.tar.bz2'],
@@ -53,7 +64,7 @@ def build_modules(sha256_dict, languages):
         'dest': 'firefox'
     }]
     for lang in languages:
-        info = {
+        sources_to_extend.append({
             'type': 'file',
             'path': f'langpacks/{lang}.xpi',
             'url': f'https://ftp.mozilla.org/pub/firefox/releases/{version}/{platform}-{arch}/xpi/{lang}.xpi',
@@ -61,21 +72,45 @@ def build_modules(sha256_dict, languages):
             'dest': 'langpacks/',
             'dest-filename': f'{lang}.xpi',
             'only-arches': [arch]
-        }
-        modules.append(info)
-    return modules
+        })
+    for item in json_object['modules']:
+        if isinstance(item, dict) and item['name'] == 'firefox':
+            item['sources'].extend(sources_to_extend)
+            break
+
+
+def merge_baseapp_modules(json_objects):
+    with open('./BaseApp/org.mozilla.firefox.BaseApp.json', 'r') as fi:
+        json_obj = json.load(fi)
+        # Fixup
+        json_obj['modules'] = map(lambda s: s.replace('dbus-glib-0.110.json', 'dbus-glib.json') if isinstance(s, str) else s, json_obj['modules'])
+        json_objects['modules'].extend(json_obj)
+
 
 if __name__ == '__main__':
-    version = current_esr_version()
-    if parse(version) <= parse(get_last_ver()):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--force_version', type=str,
+                        help="Force version", nargs='?')
+    parser.add_argument('--skip_check', action='store_true',
+                        help='Skip version check', default=False)
+    args = parser.parse_args()
+
+    if args.force_version is not None:
+        print("Forcing version {}".format(args.force_version))
+        version_str = args.force_version
+    else:
+        version_str = current_esr_version()
+    if (not args.skip_check) and parse(version_str) <= parse(get_last_ver()):
         print('No update!')
         exit(0)
     else:
-        set_last_ver(version)
-    info = get_build_info(version)
-    languages = build_language_list(version, info['build_number'])
-    sha256_dict = build_sha256dict(version)
+        set_last_ver(version_str)
 
-    modules = build_modules(sha256_dict, languages)
-    json_insert_modules('./in/org.mozilla.firefox_esr.json.template', './org.mozilla.firefox_esr.json', modules)
-    replace_vars('./in/org.mozilla.firefox_esr.appdata.xml.template', './org.mozilla.firefox_esr.appdata.xml', VERSION=version, RELEASE_TIMESTAMP=info['date'])
+    info = get_build_info(version_str)
+    languages = build_language_list(version_str, info['build_number'])
+    sha256_dict = build_sha256dict(version_str)
+
+    MyJsonTapper('./in/org.mozilla.firefox_esr.json.template').tap(extend_firefox_source,
+                                                                   sha256_dict, languages, version_str).tap(merge_baseapp_modules).output('./org.mozilla.firefox_esr.json')
+    varsubst('./in/org.mozilla.firefox_esr.appdata.xml.template',
+             './org.mozilla.firefox_esr.appdata.xml', VERSION=version_str, RELEASE_TIMESTAMP=info['date'])
