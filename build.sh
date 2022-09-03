f919e7f8b46fafdf723bd9d9a15f890e1f40b0cc
Martin Marmsoler <martin.marmsoler@gmail.com>
2022-09-03 09:06:00 +0200

    move all modules into manifest it self and use yaml instead of json

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
- flatpak-builder --force-clean --repo=UDPLoggerRepo build com.gitlab.Murmele.UDPLogger.json
+ flatpak-builder --force-clean --repo=UDPLoggerRepo build com.gitlab.Murmele.UDPLogger.yaml
