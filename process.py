1338d21f70e47003f54a2e6ab760877e19b8cf0c
HZ <99131470+hustler-not-chatty@users.noreply.github.com>
2022-09-18 23:54:15 +0800

    process.py: prepare for more arches
    template: remove dconf-related filesystem exposure

--- process.py
+++ process.py
@@ -10,7 +10,8 @@ BINARY_BASE_URL = 'https://ftp.mozilla.org/pub/firefox/releases'
 VERSIONS_API_URL = 'https://product-details.mozilla.org/1.0/firefox_versions.json'
 
 platform = 'linux'
-arch = 'x86_64'
+arches = ['x86_64']
+freedesktop_version = '21.08'
 
 
 class MyJsonTapper:
@@ -56,27 +57,36 @@ def varsubst(input_path, output_path, **vars):
 
 
 def extend_firefox_source(json_object, sha256_dict, languages, version):
-    sources_to_extend = [{
-        'type': 'archive',
-        'url': f'{BINARY_BASE_URL}/{version}/{platform}-{arch}/en-US/firefox-{version}.tar.bz2',
-        'sha256': sha256_dict[f'{platform}-{arch}/en-US/firefox-{version}.tar.bz2'],
-        'only-arches': [arch],
-        'dest': 'firefox'
-    }]
-    for lang in languages:
+    sources_to_extend = []
+    for arch in arches:
         sources_to_extend.append({
-            'type': 'file',
-            'path': f'langpacks/{lang}.xpi',
-            'url': f'https://ftp.mozilla.org/pub/firefox/releases/{version}/{platform}-{arch}/xpi/{lang}.xpi',
-            'sha256': sha256_dict[f'{platform}-{arch}/xpi/{lang}.xpi'],
-            'dest': 'langpacks/',
-            'dest-filename': f'{lang}.xpi',
-            'only-arches': [arch]
+            'type': 'archive',
+            'url': f'{BINARY_BASE_URL}/{version}/{platform}-{arch}/en-US/firefox-{version}.tar.bz2',
+            'sha256': sha256_dict[f'{platform}-{arch}/en-US/firefox-{version}.tar.bz2'],
+            'only-arches': [arch],
+            'dest': 'firefox'
         })
+        for lang in languages:
+            sources_to_extend.append({
+                'type': 'file',
+                'path': f'langpacks/{lang}.xpi',
+                'url': f'https://ftp.mozilla.org/pub/firefox/releases/{version}/{platform}-{arch}/xpi/{lang}.xpi',
+                'sha256': sha256_dict[f'{platform}-{arch}/xpi/{lang}.xpi'],
+                'dest': 'langpacks/',
+                'dest-filename': f'{lang}.xpi',
+                'only-arches': [arch]
+            })
     for item in json_object['modules']:
         if isinstance(item, dict) and item['name'] == 'firefox':
             item['sources'].extend(sources_to_extend)
             break
+
+
+def set_freedesktop_version(json_objects, version):
+    json_objects['runtime-version'] = str(version)
+    extension_root = json_objects['add-extensions']
+    for k in extension_root:
+        extension_root[k]['version'] = version
 
 
 def merge_baseapp(json_objects):
@@ -114,6 +124,6 @@ if __name__ == '__main__':
     sha256_dict = build_sha256dict(version_str)
 
     MyJsonTapper('./in/org.mozilla.firefox_esr.json.template').tap(extend_firefox_source,
-                                                                   sha256_dict, languages, version_str).tap(merge_baseapp).output('./org.mozilla.firefox_esr.json')
+                                                                   sha256_dict, languages, version_str).tap(merge_baseapp).tap(set_freedesktop_version, freedesktop_version).output('./org.mozilla.firefox_esr.json')
     varsubst('./in/org.mozilla.firefox_esr.appdata.xml.template',
              './org.mozilla.firefox_esr.appdata.xml', VERSION=version_str, RELEASE_TIMESTAMP=info['date'])
