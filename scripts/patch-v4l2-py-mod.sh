2da54ab8fc836297177f4efa8dd6203325e2861a
Dylan Turner <dylantdmt@gmail.com>
2022-09-29 00:43:43 -0500

    removed unnecessary files

--- scripts/patch-v4l2-py-mod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Author: Dylan Turner
-# Description: Patches v4l2 python module to work with modern python
-
-PY_VERS_STR=($(python --version))
-PY_VERS_W_MIN=${PY_VERS_STR[1]}
-PY_VERS=$(echo $PY_VERS_W_MIN | rev | cut -c3- | rev)
-cp scripts/v4l2\ \(patch\).py .venv/lib/python$PY_VERS/site-packages/v4l2.py
