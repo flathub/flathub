b5906b0ff0dcc04225dce9d99700e880cb70d9bc
Marco Neumann <marco@crepererum.net>
2022-10-17 21:04:57 +0200

    fix lsb release mocking

--- run.bash
+++ run.bash
@@ -5,4 +5,5 @@ set -euo pipefail
 export ELECTRON_ENABLE_LOGGING=1
 export TMPDIR="$XDG_CACHE_HOME"
 
+ln -s /app/etc/lsb-release /etc/lsb-release
 exec zypak-wrapper /app/extra/opt/Breitbandmessung/breitbandmessung "$@" --no-sandbox
