a144f62ba381cc1ed8768f37ab607989b00ddf3f
proletarius101 <proletarius101@protonmail.com>
2022-12-24 00:43:52 +0800

    update according to upstream
    https://github.com/RedisInsight/RedisInsight/blob/4faeede9563ba8efdd374345e41a8c06b9640033/electron-builder.json

--- redisinsight.sh
+++ redisinsight.sh
@@ -2,7 +2,7 @@
 
 set -oue pipefail
 
-export FLATPAK_ID="${FLATPAK_ID:-com.redis.RedisInsight}"
+export FLATPAK_ID="${FLATPAK_ID:-com.redis.RedisInsight-v2}"
 export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
 
 exec zypak-wrapper /app/redisinsight/AppRun $@
