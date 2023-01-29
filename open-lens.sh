698246c9f742cbfb85818742213877aee90af46d
proletarius101 <proletarius101@protonmail.com>
2023-01-29 23:04:46 +0800

    quote variables

--- open-lens.sh
+++ open-lens.sh
@@ -5,4 +5,4 @@ set -oue pipefail
 export FLATPAK_ID="${FLATPAK_ID:-dev.k8slens.OpenLens}"
 export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
 
-exec zypak-wrapper /app/open-lens/open-lens $@
+exec zypak-wrapper /app/open-lens/open-lens "$@"
