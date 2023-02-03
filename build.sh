13755cca0d481f847f024f119a0df9de79e9e7bb
Demir Yerli <mrquantumoff@protonmail.com>
2023-02-03 22:27:50 +0300

    Fix

--- build.sh
+++ build.sh
@@ -1,10 +1,17 @@
 ls
-tar -xpvf flutter_linux_*-stable.tar.xz
-export FLUTTER_PATH=$(pwd)/flutter
-export PATH="$PATH:$FLUTTER_PATH/bin"
-flutter pub get
-flutter build linux
 install -D dev.mrquantumoff.mcmodpackmanager.metainfo.xml /app/share/metainfo/dev.mrquantumoff.mcmodpackmanager.metainfo.xml
-install -D ./assets/icons/logohalf.png /app/share/icons/hicolor/256x256/apps/dev.mrquantumoff.mcmodpackmanager.png
+install -D logohalf.png /app/share/icons/hicolor/256x256/apps/dev.mrquantumoff.mcmodpackmanager.png
+mkdir -p /app/opt/dev.mrquantumoff.mcmodpackmanager
 install -D dev.mrquantumoff.mcmodpackmanager.desktop  /app/share/applications/dev.mrquantumoff.mcmodpackmanager.desktop
+cp GNULinuxBuild.tar.gz /app/opt/dev.mrquantumoff.mcmodpackmanager
+cd /app/opt/dev.mrquantumoff.mcmodpackmanager
 ls
+tar -xpf GNULinuxBuild.tar.gz
+rm GNULinuxBuild.tar.gz
+# mv build/*/*/*/*/*/ .
+mv build/*/*/*/*/* .
+rm -rf build
+ls /app/opt/dev.mrquantumoff.mcmodpackmanager
+chmod +x /app/opt/dev.mrquantumoff.mcmodpackmanager/mcmodpackmanager_reborn
+mkdir -p /app/bin
+ln -sf /app/opt/dev.mrquantumoff.mcmodpackmanager/mcmodpackmanager_reborn /app/bin/mcmodpackmanager_reborn
