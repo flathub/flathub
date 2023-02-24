8835916d33d32e61f6df867db62cd8a72799b73e
Nathanael <42559123+nate-xyz@users.noreply.github.com>
2023-02-24 11:43:38 -0500

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo rm -R build-dir && \
-sudo rm -R .flatpak-builder && \
-flatpak uninstall io.github.nate_xyz.Conjure && \
-flatpak-builder build-dir io.github.nate_xyz.Conjure.json && \
-flatpak-builder --user --install --force-clean build-dir io.github.nate_xyz.Conjure.json && \
-flatpak run io.github.nate_xyz.Conjure
