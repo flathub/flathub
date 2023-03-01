d89fb12a2b9fc7b2405f933584dbb54001fc431d
Álan Crístoffer <acristoffers@startmail.com>
2023-03-01 21:23:25 +0100

    Applies reviewer's change requests.

--- generate-sources.sh
+++ generate-sources.sh
@@ -16,11 +16,12 @@ poetry run flatpak-node-generator -o out2.json yarn ../../Lachesis/Lachesis/yarn
 poetry run flatpak-node-generator -o out3.json yarn ../../Lachesis/desktop/yarn.lock
 poetry run flatpak-node-generator -o out4.json yarn ../../Lachesis/src/desktop/yarn.lock
 
-jq -sc flatten out1.json out2.json out3.json out4.json > ../../tmp.json
+# jq -sc "flatten | unique | sort_by(.type)" out1.json out2.json out3.json out4.json > ../../tmp.json
+jq -sc "flatten | unique | sort_by(.type)" out1.json out2.json out3.json out4.json > ../../generated-sources.json
 
 popd || exit
 
-jq -c unique tmp.json > tmp2.json
-jq -c '[.[] | select((has("only-arches") | not) or (."only-arches" | contains(["x86_64"])))] | sort_by(.type)' tmp2.json > generated-sources.json
+# jq -c unique tmp.json > tmp2.json
+# jq -c '[.[] |  sort_by(.type)' tmp2.json > generated-sources.json
 
 rm -rf Lachesis flatpak-builder-tools tmp{,2}.json
