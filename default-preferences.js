9a7633a5931e61485aba5757f22786771181ed9f
proletarius101 <proletarius101@protonmail.com>
2023-04-06 15:20:14 +0800

    add net.mullvad.MullvadBrowser

---
+++ default-preferences.js
@@ -0,0 +1,8 @@
+pref("app.update.auto", false);
+pref("app.update.enabled", false);
+pref("app.update.autoInstallEnabled", false);
+pref("intl.locale.requested", "");
+pref("intl.locale.matchOS", true);
+pref("mail.shell.checkDefaultClient", false);
+pref("mail.shell.checkDefaultMail", false);
+pref("spellchecker.dictionary_path", "/usr/share/hunspell");
