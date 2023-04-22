62b37766994917391cac5774f9bc1212d602cd33
istori1 <107304850+istori1@users.noreply.github.com>
2023-04-22 16:47:55 -0400

    Add files via upload

---
+++ dev.lizardbyte.app.sunshine.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+PORT=47990
+
+if ! curl http://localhost:$PORT > /dev/null 2>&1; then
+  (sleep 3 && xdg-open http://localhost:$PORT) &
+  sunshine
+else
+  xdg-open http://localhost:$PORT
+fi
