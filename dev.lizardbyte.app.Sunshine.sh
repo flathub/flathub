7207af67158b41ee3d15cc2139aa4c21b785830f
istori1 <107304850+istori1@users.noreply.github.com>
2023-04-28 00:17:11 -0400

    Apply upstream prefix patch

--- dev.lizardbyte.app.Sunshine.sh
+++ dev.lizardbyte.app.Sunshine.sh
@@ -2,8 +2,8 @@
 
 PORT=47990
 
-if ! curl http://localhost:$PORT > /dev/null 2>&1; then
-  (sleep 3 && xdg-open http://localhost:$PORT) &
+if ! curl https://localhost:$PORT > /dev/null 2>&1; then
+  (sleep 3 && xdg-open https://localhost:$PORT) &
   sunshine
 else
   xdg-open https://localhost:$PORT
