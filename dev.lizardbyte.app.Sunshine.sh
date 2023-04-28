ca08cacc635e38c445fafe6d61283a452ad2a5d1
istori1 <107304850+istori1@users.noreply.github.com>
2023-04-28 01:02:51 -0400

    curl -k

--- dev.lizardbyte.app.Sunshine.sh
+++ dev.lizardbyte.app.Sunshine.sh
@@ -2,7 +2,7 @@
 
 PORT=47990
 
-if ! curl https://localhost:$PORT > /dev/null 2>&1; then
+if ! curl -k https://localhost:$PORT > /dev/null 2>&1; then
   (sleep 3 && xdg-open https://localhost:$PORT) &
   sunshine
 else
