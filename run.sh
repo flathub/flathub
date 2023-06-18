5f65e72121679c11eb769cf2f84daad6b5a599ea
marco <marco@mm.mm>
2023-06-18 23:05:45 +0100

    Address comments from PR

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-java \
-  -Djava.util.prefs.userRoot=$XDG_CONFIG_HOME \
-  -Djava.io.tmpdir=$XDG_CACHE_HOME/tmp/ \
+exec java \
+  "-Djava.util.prefs.userRoot=$XDG_CONFIG_HOME" \
+  "-Djava.io.tmpdir=$XDG_CACHE_HOME/tmp/" \
   -jar /app/bin/tambourine.jar
