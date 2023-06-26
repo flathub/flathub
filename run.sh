d1ec7e66128a9c694e612cd5a019fab90853f159
marco <marco@mm.mm>
2023-06-26 19:02:49 +0100

    Create tmp directory if missing

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+# Create tmp directory if missing
+mkdir -p "$XDG_CACHE_HOME/tmp/"
+
 exec java \
   "-Djava.util.prefs.userRoot=$XDG_CONFIG_HOME" \
   "-Djava.io.tmpdir=$XDG_CACHE_HOME/tmp/" \
