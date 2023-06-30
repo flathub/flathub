857cc20981bb60d49fa08ea9d480244822d6e9d6
doomsdayrs <doomsdayrs@gmail.com>
2023-06-30 15:58:05 -0400

    Improve main script

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-exec /app/bin/$@ -kotlin-home /app/lib/
+#!/bin/bash -e
+
+if [ -z "$1" ]; then
+	echo "No argument supplied. Possible programs:"
+	ls /app/bin/ | sed "s/run\.sh//"
+else
+	exec /app/bin/$@ -kotlin-home /app/lib/
+fi
