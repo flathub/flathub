d555161762152ee34ca72a96d21a0faa57f39da9
kevin <kevin_tee@pm.me>
2023-07-19 19:04:37 +0200

    clean up

--- generate_python_deps.sh
+++ generate_python_deps.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
-# This create some dependencies that is not generate
-python3 flatpak-pip-generator setuptools_rust hatchling rtree -o spyder_additional_deps &&
+# This generate dependencies for Spyder
+# Mark the script file as executable and run with ./generate_python_deps.sh
+
+# This create some dependencies that is missing
+python3 flatpak-pip-generator setuptools_rust hatchling -o spyder_additional_deps &&
 # Remove previous text file if any
 rm -f spyder_*.txt || true &&
 # pipgrip generate list of dependencies of spyder with pip and write it to a text file, install pipgrip with 'pip3 install pipgrip'
@@ -9,7 +12,7 @@ pipgrip spyder > spyder_pipgrip.txt &&
 cp spyder_pipgrip.txt spyder_deps.txt &&
 # Remove deps that is already installed
 sed -i -E '/^(spyder|pyqt|markupsafe|pygments|six)/d' spyder_deps.txt &&
-# Move python lib that requires rust to spyder_deps_rust.txt
+# Move python lib that requires rust to spyder_deps_rust.txt. Rust dependencies is complicate
 grep -E '^(jellyfish|jsonschema|rpds|cryptography|referencing|keyring|secretstorage|nbconvert|nbclient|nbformat|python-lsp-black|black)' spyder_deps.txt >> spyder_deps_rust.txt &&
 sed -i -E '/^(jellyfish|jsonschema|rpds|cryptography|referencing|keyring|secretstorage|nbconvert|nbclient|nbformat|python-lsp-black|black)/d' spyder_deps.txt &&
 # Generate .json file from spyder_deps.txt while ignoring some deps that is already include in the sdk
@@ -21,6 +24,6 @@ python3 flatpak-pip-generator pybind11 pyparsing==3.0.9 pillow cppy kiwisolver f
 # Generate deps for spyder terminal plugins
 python3 flatpak-pip-generator terminado tornado coloredlogs -o spyder_deps_terminal &&
 # Remove text files
-# rm -f spyder_*.txt || true &&
-# Build
-flatpak run org.flathub.flatpak-external-data-checker --edit-only *.yaml
+rm -f spyder_*.txt || true &&
+# Build the manifest, if not, just comment out
+flatpak-builder build --force-clean --install --user *.yaml
