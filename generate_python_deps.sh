2631ac836b6a79d52e1f2773dee71db90eb705f0
kevin <kevin_tee@pm.me>
2023-07-21 01:18:52 +0200

    split json manifest

--- generate_python_deps.sh
+++ generate_python_deps.sh
@@ -3,28 +3,26 @@
 # The generated files are spyder_deps_additional.json spyder_deps.json spyder_deps_numerical.json spyder_deps_rust.json spyder_deps_terminal.json
 # Mark the script file as executable and run with ./generate_python_deps.sh
 
-# This create some dependencies that is missing
-python3 flatpak-pip-generator setuptools_rust hatchling -o spyder_deps_additional &&
-# Remove previous text file if any
-rm -f spyder_*.txt || true &&
-# pipgrip generate list of dependencies of spyder with pip and write it to a text file, install pipgrip with 'pip3 install pipgrip'
-pipgrip spyder > spyder_pipgrip.txt &&
-# Create a copy and we will work with the copy, pipgrip take a long time
-cp spyder_pipgrip.txt spyder_deps.txt &&
-# Remove deps that is already installed
-sed -i -E '/^(spyder|pyqt|markupsafe|pygments|six)/d' spyder_deps.txt &&
+
+python3 flatpak-pip-generator setuptools_rust hatchling -o spyder_deps_additional && # This create some dependencies that is missing
+rm -f spyder_*.txt || true && # Remove previous text file if any
+pipgrip spyder > spyder_pipgrip.txt && # pipgrip generate list of dependencies of spyder with pip and write it to a text file, install pipgrip with 'pip3 install pipgrip'
+cp spyder_pipgrip.txt spyder_deps_1.txt && # Create a copy and we will work with the copy, pipgrip take a long time
+sed -i -E '/^(spyder|pyqt|markupsafe|pygments|six)/d' spyder_deps_1.txt && # Remove deps that is already installed
 # Move python lib that requires rust to spyder_deps_rust.txt. Rust dependencies is complicated
-grep -E '^(jellyfish|jsonschema|rpds|cryptography|referencing|keyring|secretstorage|nbconvert|nbclient|nbformat|python-lsp-black|black)' spyder_deps.txt >> spyder_deps_rust.txt &&
-sed -i -E '/^(jellyfish|jsonschema|rpds|cryptography|referencing|keyring|secretstorage|nbconvert|nbclient|nbformat|python-lsp-black|black)/d' spyder_deps.txt &&
-# Generate .json file from spyder_deps.txt while ignoring some deps that is already include in the sdk
-python3 flatpak-pip-generator --requirements-file spyder_deps.txt --ignore-installed MarkupSafe,pygments,six -o spyder_deps &&
+grep -E '^(jellyfish|jsonschema|rpds|cryptography|referencing|keyring|secretstorage|nbconvert|nbclient|nbformat|python-lsp-black|black)' spyder_deps_1.txt >> spyder_deps_rust.txt &&
+sed -i -E '/^(jellyfish|jsonschema|rpds|cryptography|referencing|keyring|secretstorage|nbconvert|nbclient|nbformat|python-lsp-black|black)/d' spyder_deps_1.txt &&
+# The spyder_deps_1.txt will generate too large of a json file so split them to spyder_deps_2.txt
+sed -n '1,60p' spyder_deps_1.txt > spyder_deps_1_temp.txt  # Save the first 10 lines of fileA to a temporary file
+sed -n '61,$p' spyder_deps_1.txt > spyder_deps_2.txt      # Save lines 11 and beyond to fileB
+mv spyder_deps_1_temp.txt spyder_deps_1.txt               # Overwrite fileA with the first 10 lines from the temporary file
+# Generate .json file from spyder_deps_1.txt while ignoring some deps that is already include in the sdk
+python3 flatpak-pip-generator --requirements-file spyder_deps_1.txt --ignore-installed MarkupSafe,pygments,six -o spyder_deps_1 &&
+python3 flatpak-pip-generator --requirements-file spyder_deps_2.txt --ignore-installed MarkupSafe,pygments,six -o spyder_deps_2 &&
 # Generate deps with req2flatpak for precompile lib because build from source need rust deps, install req2flatpak with 'pip3 install req2flatpak'
 req2flatpak --requirements-file spyder_deps_rust.txt --target-platforms 310-x86_64 310-aarch64 --outfile spyder_deps_rust.json &&
-# Generate recommended deps for some numerical libs for spyder
+# Generate recommended deps for some numerical libs for spyder, Matplotlib have issue building with newer pyparsing
 python3 flatpak-pip-generator pybind11 pyparsing==3.0.9 pillow cppy kiwisolver fonttools cycler meson-python contourpy openpyxl versioneer pandas pythran scipy sympy statsmodels --ignore-installed MarkupSafe,pygments,six -o spyder_deps_numerical &&
-# Generate deps for spyder terminal plugins
-python3 flatpak-pip-generator terminado tornado coloredlogs -o spyder_deps_terminal &&
-# Remove text files
-rm -f spyder_*.txt || true &&
-# Build the manifest, if not, just comment out
-flatpak-builder build --force-clean --install --user *.yaml
+python3 flatpak-pip-generator terminado tornado coloredlogs -o spyder_deps_terminal && # Generate deps for spyder terminal plugins
+rm -f spyder_*.txt || true && # Remove text files
+flatpak-builder build --force-clean --install --user *.yaml # Build the manifest, if not, just comment out
