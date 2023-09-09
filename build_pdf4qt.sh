c900b0a500f57310c6c498db292a54578bde60aa
Jakub Melka <Mgr.Jakub.Melka@gmail.com>
2023-09-09 15:09:06 +0200

    Update

--- build_pdf4qt.sh
+++ build_pdf4qt.sh
@@ -8,23 +8,30 @@ cd vcpkg
 cd ..
 export VCPKG_ROOT=$(pwd)/vcpkg
 
-# Instalace závislostí
+# Install tbb
 ${VCPKG_ROOT}/vcpkg install tbb
 
-# Sestavení vaší aplikace
+# Build
 cd pdf4qt
 export CMAKE_BUILD_PARALLEL_LEVEL=$(nproc)
 cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -DPDF4QT_INSTALL_QT_DEPENDENCIES=0 -DPDF4QT_LINK_TBB=ON -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake -DCMAKE_INSTALL_PREFIX=/app
 cmake --build build
 cmake --install build
 
-mv -f /app/lib/*Plugin.so*  /app/lib/pdf4qt/
-mv -f /app/usr/share/icons/*.svg /app/share/icons
-mv -f /app/usr/share/applications/*.desktop /app/share/applications
+# Move icons and desktop files to the right place
+cd ..
 
-rmdir /app/usr/share/icons
-rmdir /app/usr/share/applications
-rmdir /app/usr/share
-rmdir /app/usr
+mkdir -p app/lib/pdf4qt
+mkdir -p app/share/icons
+mkdir -p app/share/applications
 
-sed -i 's|/usr/|/app/|g' /app/share/applications/*.desktop
+mv -f app/lib/*Plugin.so*  app/lib/pdf4qt/
+mv -f app/usr/share/icons/*.svg app/share/icons/
+mv -f app/usr/share/applications/*.desktop app/share/applications/
+
+rmdir app/usr/share/icons
+rmdir app/usr/share/applications
+rmdir app/usr/share
+rmdir app/usr
+
+sed -i 's|/usr/|/app/|g' app/share/applications/*.desktop
