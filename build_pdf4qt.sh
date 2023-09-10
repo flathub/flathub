db144da46d0f557e0621a835480d9b3ba40585f9
Jakub Melka <Mgr.Jakub.Melka@gmail.com>
2023-09-10 13:42:46 +0200

    Desktop icons

--- build_pdf4qt.sh
+++ build_pdf4qt.sh
@@ -36,4 +36,26 @@ rmdir /app/usr/share/applications
 rmdir /app/usr/share
 rmdir /app/usr
 
-sed -i 's|/usr/|/app/|g' /app/share/applications/*.desktop
+# Edit the files
+
+sed -i 's|/usr/bin/||g' /app/share/applications/*.desktop
+sed -i 's|/usr/share/icons/||g' /app/share/applications/*.desktop
+#sed -i 's|.svg||g' /app/share/applications/*.desktop
+
+# Rename files - flatpak needs prefix io.github.JakubMelka.Pdf4qt, if
+# file has to be exported.
+mv -f /app/share/applications/Pdf4QtDocDiff.desktop /app/share/applications/io.github.JakubMelka.Pdf4qt.Pdf4QtDocDiff.desktop
+mv -f /app/share/applications/Pdf4QtDocPageOrganizer.desktop /app/share/applications/io.github.JakubMelka.Pdf4qt.Pdf4QtDocPageOrganizer.desktop
+mv -f /app/share/applications/Pdf4QtViewerLite.desktop /app/share/applications/io.github.JakubMelka.Pdf4qt.Pdf4QtViewerLite.desktop
+mv -f /app/share/applications/Pdf4QtViewerProfi.desktop /app/share/applications/io.github.JakubMelka.Pdf4qt.Pdf4QtViewerProfi.desktop
+
+mv -f /app/share/icons/Pdf4QtDocDiff.svg /app/share/icons/io.github.JakubMelka.Pdf4qt.Pdf4QtDocDiff.svg
+mv -f /app/share/icons/Pdf4QtDocPageOrganizer.svg /app/share/icons/io.github.JakubMelka.Pdf4qt.Pdf4QtDocPageOrganizer.svg
+mv -f /app/share/icons/Pdf4QtViewerLite.svg /app/share/icons/io.github.JakubMelka.Pdf4qt.Pdf4QtViewerLite.svg
+mv -f /app/share/icons/Pdf4QtViewerProfi.svg /app/share/icons/io.github.JakubMelka.Pdf4qt.Pdf4QtViewerProfi.svg
+
+# Rename icons in desktop files
+sed -i 's|Pdf4QtDocDiff.svg|io.github.JakubMelka.Pdf4qt.Pdf4QtDocDiff.svg|g' /app/share/applications/io.github.JakubMelka.Pdf4qt.Pdf4QtDocDiff.desktop
+sed -i 's|Pdf4QtDocPageOrganizer.svg|io.github.JakubMelka.Pdf4qt.Pdf4QtDocPageOrganizer.svg|g' /app/share/applications/io.github.JakubMelka.Pdf4qt.Pdf4QtDocPageOrganizer.desktop
+sed -i 's|Pdf4QtViewerLite.svg|io.github.JakubMelka.Pdf4qt.Pdf4QtViewerLite.svg|g' /app/share/applications/io.github.JakubMelka.Pdf4qt.Pdf4QtViewerLite.desktop
+sed -i 's|Pdf4QtViewerProfi.svg|io.github.JakubMelka.Pdf4qt.Pdf4QtViewerProfi.svg|g' /app/share/applications/io.github.JakubMelka.Pdf4qt.Pdf4QtViewerProfi.desktop
