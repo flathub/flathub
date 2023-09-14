fa8f41aaf816131d00faa0660ed591267002ee2c
Kavin Teenakul <andythe_great@pm.me>
2023-09-14 16:23:08 +0200

    update python deps

--- generate_python_deps.sh
+++ generate_python_deps.sh
@@ -13,16 +13,16 @@ sed -i -E '/^(spyder|pyqt|markupsafe|pygments|six)/d' spyder_deps_1.txt && # Rem
 grep -E '^(jellyfish|jsonschema|rpds|cryptography|referencing|keyring|secretstorage|nbconvert|nbclient|nbformat|python-lsp-black|black)' spyder_deps_1.txt >> spyder_deps_rust.txt &&
 sed -i -E '/^(jellyfish|jsonschema|rpds|cryptography|referencing|keyring|secretstorage|nbconvert|nbclient|nbformat|python-lsp-black|black)/d' spyder_deps_1.txt &&
 # The spyder_deps_1.txt will generate too large of a json file so split them to spyder_deps_2.txt
-sed -n '1,60p' spyder_deps_1.txt > spyder_deps_1_temp.txt  # Save the first 10 lines of fileA to a temporary file
-sed -n '61,$p' spyder_deps_1.txt > spyder_deps_2.txt      # Save lines 11 and beyond to fileB
-mv spyder_deps_1_temp.txt spyder_deps_1.txt               # Overwrite fileA with the first 10 lines from the temporary file
+sed -n '1,60p' spyder_deps_1.txt > spyder_deps_1_temp.txt  # Save the first 60 lines of fileA to a temporary file
+sed -n '61,$p' spyder_deps_1.txt > spyder_deps_2.txt      # Save lines 61 and beyond to fileB
+mv spyder_deps_1_temp.txt spyder_deps_1.txt               # Overwrite fileA with the first 60 lines from the temporary file
 # Generate .json file from spyder_deps_1.txt while ignoring some deps that is already include in the sdk
 python3 flatpak-pip-generator --requirements-file spyder_deps_1.txt --ignore-installed MarkupSafe,pygments,six -o spyder_deps_1 &&
 python3 flatpak-pip-generator --requirements-file spyder_deps_2.txt --ignore-installed MarkupSafe,pygments,six -o spyder_deps_2 &&
 # Generate deps with req2flatpak for precompile lib because build from source need rust deps, install req2flatpak with 'pip3 install req2flatpak'
 req2flatpak --requirements-file spyder_deps_rust.txt --target-platforms 310-x86_64 310-aarch64 --outfile spyder_deps_rust.json &&
 # Generate recommended deps for some numerical libs for spyder, Matplotlib have issue building with newer pyparsing
-python3 flatpak-pip-generator pybind11 pyparsing==3.0.9 pillow cppy kiwisolver fonttools cycler meson-python contourpy openpyxl versioneer pandas pythran scipy sympy statsmodels --ignore-installed MarkupSafe,pygments,six -o spyder_deps_numerical &&
+python3 flatpak-pip-generator pybind11 pyparsing pillow cppy kiwisolver fonttools cycler meson-python contourpy openpyxl versioneer pandas pythran scipy sympy statsmodels --ignore-installed MarkupSafe,pygments,six -o spyder_deps_numerical &&
 python3 flatpak-pip-generator terminado tornado coloredlogs -o spyder_deps_terminal && # Generate deps for spyder terminal plugins
 rm -f spyder_*.txt || true && # Remove text files
-flatpak-builder build --force-clean --install --user *.yaml # Build the manifest, if not, just comment out
+flatpak-builder build --force-clean --install --user org.spyder_ide.spyder.yaml # Build the manifest, if not, just comment out
