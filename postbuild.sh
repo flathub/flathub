3609d966da43a52c01487e17a056a59d528dbc21
Jakub Melka <Mgr.Jakub.Melka@gmail.com>
2023-09-16 19:48:35 +0200

    Remove old build script

--- postbuild.sh
+++ postbuild.sh
@@ -4,3 +4,5 @@ set -e
 mkdir -p /app/lib/pdf4qt
 mv -f /app/lib/*Plugin.so*  /app/lib/pdf4qt/
 
+cp /app/share/icons/io.github.JakubMelka.Pdf4qt.Pdf4QtViewerProfi.svg /app/share/icons/io.github.JakubMelka.Pdf4qt.svg
+cp /app/share/icons/hicolor/128x128/apps/io.github.JakubMelka.Pdf4qt.Pdf4QtViewerProfi.png /app/share/icons/hicolor/128x128/apps/io.github.JakubMelka.Pdf4qt.png
