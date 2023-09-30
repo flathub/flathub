daecd8a96c769760951bddf6790fcaefa15b3495
Marc0x1 <marco.loletti@zohomail.eu>
2023-09-30 17:12:17 +0200

    apply suggestions

--- amplide.sh
+++ amplide.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
-/app/extra/amplide/amplide
+
+cd $XDG_DATA_HOME
+if [ ! -d "ampl.linux-intel64" ]
+then
+
+    (
+        cp -r "/app/extra/ampl.linux-intel64" . ;
+    ) |
+    zenity --progress \
+    --title="AMPL IDE" \
+    --text="Configuring the application" \
+    --percentage=0 \
+    --auto-close \
+    --auto-kill
+
+    (( $? != 0 )) && zenity --error --text="Error while copying the client."
+
+fi
+exec ampl.linux-intel64/amplide/amplide "$@"
