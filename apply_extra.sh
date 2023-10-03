2228956a3b339635a7aa7fc55bdaf412fb367469
MadDeCoDeR <gkwin8ac@hotmail.gr>
2023-10-03 23:41:07 +0300

    Add tag to main repo and x-checker-data for BFA-Assets

--- apply_extra.sh
+++ apply_extra.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/sh
-unzip BFA-Assets-1.3.0.zip
+unzip BFA-Assets.zip
 mkdir -p games/doombfa/base
 mkdir -p games/doombfa/base_BFG
-cp -R BFA-Assets-1.3.0/base/* games/doombfa/base
-cp -R BFA-Assets-1.3.0/base_BFG/* games/doombfa/base_BFG
+cp -R base/* games/doombfa/base
+cp -R base_BFG/* games/doombfa/base_BFG
 cp -R /app/share/games/doombfa/* games/doombfa
 rm -r games/doombfa/base/renderprogs
-rm -r BFA-Assets-1.3.0
-rm BFA-Assets-1.3.0.zip
+rm -r base
+rm -r base_BFG
+rm .gitignore
+rm *.vdf
+rm *.md
+rm BFA-Assets.zip
 rm games/doombfa/base/generate_zBFA_s1.cfg
 rm games/doombfa/base/generate_zBFA_s2.cfg
 rm games/doombfa/base/DoomBFA.sh
