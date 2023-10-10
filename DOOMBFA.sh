c8521de57ebd823f417d71f77cbc31fddf898e7e
MadDeCoDeR <gkwin8ac@hotmail.gr>
2023-10-10 22:40:40 +0300

    Move save folder to persist and fix issue with launcher

--- DOOMBFA.sh
+++ DOOMBFA.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [[ " $@ " =~ " -open-launcher " ]]
 then
-   mkdir -p "$HOME/.doombfa/base/lib"
-   export LD_LIBRARY_PATH="$HOME/.doombfa/base/lib"
-   mono /app/bin/CDL.exe $*
+   mkdir -p "$XDG_DATA_HOME/.doombfa/base/lib"
+   export LD_LIBRARY_PATH="$XDG_DATA_HOME/.doombfa/base/lib"
+   (cd "$XDG_DATA_HOME/.doombfa" && mono /app/bin/CDL.exe $*)
 else
-   mkdir -p "$HOME/.doombfa/base/lib"
-   export LD_LIBRARY_PATH="$HOME/.doombfa/base/lib"
-   (cd "$HOME/.doombfa/base/lib" && exec /app/bin/DoomBFA $*)
+   mkdir -p "$XDG_DATA_HOME/.doombfa/base/lib"
+   export LD_LIBRARY_PATH="$XDG_DATA_HOME/.doombfa/base/lib"
+   (cd "$XDG_DATA_HOME/.doombfa/base/lib" && exec /app/bin/DoomBFA $*)
 fi
