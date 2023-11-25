c42ea759b673196490bbbbccfff5bd9d95213086
bztd <wntex@yandex.com>
2023-11-25 16:56:17 -0500

    0

---
+++ lyx-sh.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+directory="${HOME}/.lyx"
+_pwd=$PWD
+
+Init(){
+	
+	echo "sh --> Starting LyX ..."
+	lyx
+}
+
+Prepare() {
+	
+		echo "sh --> Directory $directory is empty, executing: configure.py"
+		cd $directory
+		python3 /app/share/lyx/configure.py
+		echo "sh --> Copying lyxrc.defaults ..."
+		cp /app/temp/lyxrc.defaults ${directory}/
+		cd $_pwd
+		
+		Init
+    
+}
+
+if [ -d "$directory" ]; then
+
+    if test -z "$(ls -A $directory)"; then
+		Prepare
+    else
+        Init
+    fi
+
+else
+    mkdir $directory
+    Prepare
+fi
+
