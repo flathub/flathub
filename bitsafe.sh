4e730603ffd76d2601662ae36a4220d9b88dcc89
francivaldo4334 <francivaldodev@gmail.com>
2023-12-18 12:44:41 -0300

    feat

--- bitsafe.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export PYTHONPATH="/app/packages/bitsafe"
-
-python3 -m bitsafe
--- bitsafe/__main__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from bitsafe.bitsafe_cli import BitSafeCLI
-
-if __name__ == "__main__":
-    bsafe = BitSafeCLI()
