6f340cf9afbc02864dbd9ba7a0e8c76c9afadacb
Matthew Polk <153921961+exjaynine@users.noreply.github.com>
2023-12-18 10:59:19 -0500

    Update writersolo.sh

--- writersolo.sh
+++ writersolo.sh
@@ -4,4 +4,4 @@
 #This is literally the executable that that gets ran. It's been a major headache getting this to work so-
 #I'm just going to keep the executable binary name as is. Feel free to change it later to something like-
 #'writersolo-desktop' or anything you like.
-zypak-wrapper '/app/extra/opt/WriterSolo/${productName}-${version}-${arch}.${ext}'
+zypak-wrapper '/app/extra/opt/WriterSolo/writersolo-desktop'
