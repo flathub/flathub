3c807cfede5d733f2f565636fd167b72413e8596
Matthew Polk <153921961+exjaynine@users.noreply.github.com>
2023-12-18 05:28:10 -0500

    Update apply_extra.sh

--- apply_extra.sh
+++ apply_extra.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/bash
 
-
-bsdtar -xf writersolo.deb
-tar -xvf data.tar.xz
+bsdtar -Oxf writersolo.deb 'data.tar.xz'|bsdtar -xvf - --exclude='usr'
 
 rm writersolo.deb
-rm *.xz
-rm *.gz
-rm debian-binary
 
-#Note, not removing the usr/ directory it comes with. At least, not right now.
-
-
-
-
-#SUID chrome-sandbox for Electron 5+
-chmod 4755 ./opt/WriterSolo/chrome-sandbox
 
 
 #DONE!
