cf3bc044d61bf65623b7e40d0179f767e2e8474c
flexagoon <code@fxgn.dev>
2023-12-29 18:56:41 +0300

    don't extract icons of extra sizes

--- apply_extra.sh
+++ apply_extra.sh
@@ -7,10 +7,5 @@ unappimage httpie.appimage
 # Move all the data to /app/extra/httpie
 mv squashfs-root httpie
 
-# Install icons
-for size in 16x16 32x32 64x64 128x128 256x256 512x512 1024x1024; do
-    install -Dm644 "httpie/usr/share/icons/hicolor/${size}/apps/httpie.png" "/app/extra/export/share/icons/hicolor/${size}/apps/io.httpie.Httpie.png"
-done
-
 # Clean up
 rm httpie.appimage
