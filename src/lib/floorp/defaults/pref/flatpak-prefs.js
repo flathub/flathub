4074bd59916cb18ba976efaba8dce6d45d4645c5
typeling1578 <pub@typeling1578.dev>
2024-02-21 10:20:35 +0900

    11.10.2.2, Update PCSC from 2.0.0 to 2.0.1, Disable built-in updater

--- src/lib/floorp/defaults/pref/flatpak-prefs.js
+++ src/lib/floorp/defaults/pref/flatpak-prefs.js
@@ -1,4 +1,7 @@
 pref("distribution.about", "Floorp for Flatpak\nContributors: nexryai, typeling1578,\ndominichayesferen");
 pref("distribution.id", "one.ablaze.floorp");
 
+pref("enable.floorp.update", false);
+pref("enable.floorp.updater.latest", false);
+
 pref("accessibility.typeaheadfind.soundURL", "default");
