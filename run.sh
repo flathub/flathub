88aa06e274883ab9677ad831ff660cb97fedae8b
Corvonn <112516229+Corvonn@users.noreply.github.com>
2024-03-29 15:10:31 +0100

    added Discord rich presence support (hopefully)

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+
+# For Discord RP support. 
+# Source: https://github.com/flathub/com.discordapp.Discord/wiki/Rich-Precense-(discord-rpc)#flatpak-applications
+for i in {0..9}; do
+    test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
+done
+
 exec /app/extra/launcher/labymodlauncher
