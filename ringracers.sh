485844a6827bae21de81a0461387e944944ab51d
Eidolon <furyhunter600@gmail.com>
2024-04-24 22:39:09 -0500

    Add org.kartkrew.RingRacers

---
+++ ringracers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in {0..9}; do
+	test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
+done
+
+if [ -z "${RINGRACERSWADDIR}" ]; then
+	export RINGRACERSWADDIR=/app/ringracers-data
+fi
+
+exec ringracers "$@"
