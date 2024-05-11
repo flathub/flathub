5761fbbaceb028ae65afa068359b996a20b40b1c
Jan Prochazka <jan.prochazka@gmail.com>
2024-05-11 08:26:19 +0200

    working image

--- dbgate.sh
+++ dbgate.sh
@@ -5,4 +5,4 @@ set -oue pipefail
 export FLATPAK_ID="${FLATPAK_ID:-org.dbgate.DbGate}"
 export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
 
-exec zypak-wrapper /app/dbgate/dbgate-bin $@
+exec zypak-wrapper /app/dbgate/dbgate $@
