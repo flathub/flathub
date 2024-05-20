6d967c58ad8412465c786d8ac60ef88e9899051e
Alejandro Perez <alejandro.perez@scanoss.com>
2024-05-20 15:23:04 -0300

    Updates: Making metainfo and icon available upstream Removing unused  permission /tmp on app manifest Unifying conditionals on run.sh

--- run.sh
+++ run.sh
@@ -2,28 +2,26 @@
 declare -a EXTRA_FLAGS=()
 
 # Display Socket
-if [[ $XDG_SESSION_TYPE == "wayland" ]]
-then
-    WAYLAND_SOCKET=${WAYLAND_DISPLAY:-"wayland-0"}
+WAYLAND_SOCKET=${WAYLAND_DISPLAY:-"wayland-0"}
 
-    if [[ -e "$XDG_RUNTIME_DIR/${WAYLAND_SOCKET}" ]]
-    then
-        echo "Wayland socket is available, running natively on Wayland."
-        echo "To disable, remove the --socket=wayland permission."
-        EXTRA_FLAGS+=(
-            "--enable-features=UseOzonePlatform,WaylandWindowDecorations"            
-            "--ozone-platform=wayland"
-            "--ozone-platform-hint=auto"            
-        )        
-    else
-        echo "Wayland socket not available, running through Xwayland."
-    fi
+if [[ $XDG_SESSION_TYPE == "wayland" && -e "$XDG_RUNTIME_DIR/${WAYLAND_SOCKET}" ]]
+then
+    
+    echo "Wayland socket is available, running natively on Wayland."
+    echo "To disable, remove the --socket=wayland permission."
+    EXTRA_FLAGS+=(
+        "--enable-features=WaylandWindowDecorations"                    
+        "--ozone-platform-hint=auto"            
+    )        
+   
     if [[ -c /dev/nvidia0 ]]
     then
         echo "Using NVIDIA on Wayland, applying workaround"
         EXTRA_FLAGS+=("--disable-gpu-sandbox")
     fi
 
+else
+    echo "Wayland socket not available, running through Xwayland."
 fi
 
 cd /app/lib/sbom-workbench
