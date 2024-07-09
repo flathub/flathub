e5e34e135e527d28f1cc29a89bcf37e5a5a7109f
bztd <117551043+bztd@users.noreply.github.com>
2024-07-09 18:45:29 -0500

    Update startTeXmacs.sh

--- startTeXmacs.sh
+++ startTeXmacs.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
 architecture_type=$(uname -m)
+perl_version=$(ls /app/texlive/lib/perl5/site_perl/)
 
 export PATH=/app/texlive/bin:/app/texlive/bin/${architecture_type}-linux:${PATH}
-
-perl_version=$(perl -e "print substr($^V, 1)")
-
 export LD_LIBRARY_PATH=/app/texlive/lib:/app/texlive/lib/perl5/${perl_version}/${architecture_type}-linux/CORE
 export PERL5LIB=/app/texlive/lib/perl5/${perl_version}:/app/texlive/lib/perl5/site_perl/${perl_version}
 export ASPELL_CONF='dict-dir /app/share/dicts'
