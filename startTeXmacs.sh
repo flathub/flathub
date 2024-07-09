920c2e5d91e6e6fc35b5000dd2f1376339ecbad1
bztd <117551043+bztd@users.noreply.github.com>
2024-07-09 17:16:57 -0500

    Add files via upload

---
+++ startTeXmacs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+architecture_type=$(uname -m)
+perl_version=$(perl -e "print substr($^V, 1)")
+
+export PATH=/app/texlive/bin:/app/texlive/bin/${architecture_type}-linux:${PATH}
+export LD_LIBRARY_PATH=/app/texlive/lib:/app/texlive/lib/perl5/${perl_version}/${architecture_type}-linux/CORE
+export PERL5LIB=/app/texlive/lib/perl5/${perl_version}:/app/texlive/lib/perl5/site_perl/${perl_version}
+export ASPELL_CONF='dict-dir /app/share/dicts'
+
+
+exec texmacs "$@"
