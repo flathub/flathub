b76903ead45ffce316da7e86dcd367e81aa24dd8
bztd <117551043+bztd@users.noreply.github.com>
2024-07-09 19:11:30 -0500

    Update startTeXmacs.sh

--- startTeXmacs.sh
+++ startTeXmacs.sh
@@ -3,6 +3,11 @@
 architecture_type=$(uname -m)
 perl_version=$(ls /app/texlive/lib/perl5/site_perl/)
 
+if ! [[ $perl_version =~ ^[0-9.]+$ ]]; then
+  echo " startTeXmacs.sh: Possible error in environment variables."
+  echo " "
+fi
+
 export PATH=/app/texlive/bin:/app/texlive/bin/${architecture_type}-linux:${PATH}
 export LD_LIBRARY_PATH=/app/texlive/lib:/app/texlive/lib/perl5/${perl_version}/${architecture_type}-linux/CORE
 export PERL5LIB=/app/texlive/lib/perl5/${perl_version}:/app/texlive/lib/perl5/site_perl/${perl_version}
