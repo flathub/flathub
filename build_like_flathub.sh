60a5e193e8ebdf6ea080e410005847dbfc32dad9
Hannes Kuchelmeister <hannes@kuchelmeister.org>
2024-07-16 23:05:52 +0200

    do not use the cache when running script

--- build_like_flathub.sh
+++ build_like_flathub.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-flatpak-builder --install appdir org.kuchelmeister.ToolboxTuner.json --force-clean --user -y
+flatpak-builder --install appdir org.kuchelmeister.ToolboxTuner.json --force-clean --disable-cache --user -y
 
