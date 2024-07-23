229e2737abb4f69bf943f44ff6102b24dab20f7d
QCanvas <QCanvas@noreply.codeberg.org>
2024-07-23 19:22:56 +1000

    Fix updater exiting when up to date

--- update.sh
+++ update.sh
@@ -2,12 +2,12 @@
 
 pyenv local 3.11
 
-pdm run utils/flatpak-pip-generator.py qcanvas==1.0.6a7 --arch-dependent-force
+# pdm run utils/flatpak-pip-generator.py qcanvas==1.0.6a7 --arch-dependent-force
 pdm run utils/metainfo-version-updater.py resources/io.github.qcanvas.QCanvasApp.metainfo.xml
 
 cd resources || exit
-git add io.github.qcanvas.QCanvasApp.metainfo.xml || exit
-git commit -m "Update metainfo" || exit
+git commit -m "Update metainfo"
 git push
+cd ..
 
 pdm run utils/update-meta-repo.py resources io.github.qcanvas.QCanvasApp.yaml
