0a9dae1d82f8ade99cd28e116724962a6db53af2
QCanvas <QCanvas@noreply.codeberg.org>
2024-07-23 19:54:28 +1000

    Rename and move nudge script

--- update.sh
+++ update.sh
@@ -3,7 +3,8 @@ APP_ID=io.github.qcanvas.QCanvasApp
 
 pyenv local 3.11
 
-./nudge-pip
+# Pypi doesn't always generate package information until someone installs it
+utils/nudge-pypi
 
 pdm run utils/flatpak-pip-generator.py qcanvas --arch-dependent-force
 pdm run utils/metainfo-version-updater.py resources/$APP_ID.metainfo.xml
