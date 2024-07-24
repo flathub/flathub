82f4a9e62b18b7df2f0c4777d0692b3d0e210449
QCanvas <QCanvas@noreply.codeberg.org>
2024-07-24 15:04:34 +1000

    Use req2flatpak instead of pip generator

--- update.sh
+++ update.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/bash
-APP_ID=io.github.qcanvas.QCanvasApp
 
-pyenv local 3.11
-
-# Pypi doesn't always generate package information until someone installs it
-utils/nudge-pypi
-
-# Doesn't work without arch-dependent-force!
-pdm run utils/flatpak-pip-generator.py qcanvas --arch-dependent-force
-# pdm run utils/metainfo-version-updater.py qcanvas $APP_ID.metainfo.xml
-
-if [ "$1" != "no-commit" ]; then
-    git add python3-qcanvas.json $APP_ID.metainfo.xml $APP_ID.yaml
-    git commit -m "Update manifest"
-fi
-
+echo "fix me"
+# APP_ID=io.github.qcanvas.QCanvasApp
+#
+# pyenv local 3.11
+#
+# # Pypi doesn't always generate package information until someone installs it
+# utils/nudge-pypi
+#
+# # Doesn't work without arch-dependent-force!
+# pdm run utils/flatpak-pip-generator.py qcanvas --arch-dependent-force
+# # pdm run utils/metainfo-version-updater.py qcanvas $APP_ID.metainfo.xml
+#
+# if [ "$1" != "no-commit" ]; then
+#     git add python3-qcanvas.json $APP_ID.metainfo.xml $APP_ID.yaml
+#     git commit -m "Update manifest"
+# fi
+#
