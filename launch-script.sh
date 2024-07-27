61a279bd38f5bb7a6d2049320c5b7f428d1c6810
Mauro Balades <mauro.balades@tutanota.com>
2024-07-27 15:03:20 +0200

    Refactor launch-script.sh for improved performance and readability

--- launch-script.sh
+++ launch-script.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-export TMPDIR=$XDG_CACHE_HOME/tmp
-
-##
-## Enable Wayland backend?
-##
-if ! [ $MOZ_DISABLE_WAYLAND ] && [ "$WAYLAND_DISPLAY" ]; then
-  if [ "$XDG_CURRENT_DESKTOP" == "GNOME" ]; then
-    export MOZ_ENABLE_WAYLAND=1
-  fi
-##  Enable Wayland on KDE/Sway
-##
-  if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
-    export MOZ_ENABLE_WAYLAND=1
-  fi
-fi
 
 exec /app/zen/zen "$@"
