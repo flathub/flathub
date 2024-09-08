66c87c5fa581325cf5f21eac6cfb0db59c8abe52
Chris Hubbard <chris@thehubbards.org>
2024-09-08 16:51:33 -0400

    Reorganize manifest

--- sab-run.sh
+++ sab-run.sh
@@ -2,4 +2,4 @@
 # Scripture App Builder
 # Command line app build and launch wrapper
 # Use sab.sh -? or see the document 'Building Apps' for more information
-exec /app/jre/bin/java -Djava.io.tmpdir="/var/tmp" --module-path "/app/lib/sdk" --add-modules javafx.web,javafx.fxml,javafx.swing,javafx.media --add-opens=javafx.fxml/javafx.fxml=ALL-UNNAMED -jar /app/scripture-app-builder/bin/scripture-app-builder.jar "$@"
+exec /app/jre/bin/java -Djava.io.tmpdir="/var/tmp" --module-path "/app/lib/openjfx" --add-modules javafx.web,javafx.fxml,javafx.swing,javafx.media --add-opens=javafx.fxml/javafx.fxml=ALL-UNNAMED -jar /app/scripture-app-builder/bin/scripture-app-builder.jar "$@"
