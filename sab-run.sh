8f7ec1677cba426f19229b14a8ec814ee667799d
Chris Hubbard <chris@thehubbards.org>
2024-09-09 18:35:44 -0400

    Remove unneeded files

--- sab-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Scripture App Builder
-# Command line app build and launch wrapper
-# Use sab.sh -? or see the document 'Building Apps' for more information
-exec /app/jre/bin/java -Djava.io.tmpdir="/var/tmp" --module-path "/app/lib/openjfx" --add-modules javafx.web,javafx.fxml,javafx.swing,javafx.media --add-opens=javafx.fxml/javafx.fxml=ALL-UNNAMED -jar /app/scripture-app-builder/bin/scripture-app-builder.jar "$@"
