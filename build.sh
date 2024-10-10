0081a2a2297227696d126ce88c3c7413f3a6fb49
Peter Severin <peter@wireframesketcher.com>
2024-10-10 16:16:21 +0300

    Update manifest as per reviewers request

--- build.sh
+++ build.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/env sh
 
 set -e
-
-install -Dm755 apply_extra ${FLATPAK_DEST}/bin/apply_extra
+set -x
 
 bsdtar -Oxf sketcher.deb 'data.tar.*' |
   bsdtar -xf - \
     --strip-components=2 \
     --exclude='./etc/' \
     --exclude='./usr/bin/' \
-    --exclude='./usr/share/doc/' \
-    --exclude='./opt/WireframeSketcher/'
+    --exclude='./usr/share/doc/'
+
+mv WireframeSketcher ${FLATPAK_DEST}/
 
 find share -type f -exec install -Dm644 "{}" "${FLATPAK_DEST}/{}" \;
 
 install -Dm644 flatpak-dev-shim.jar ${FLATPAK_DEST}/flatpak-dev-shim.jar
 install -Dm644 flatpak-dev-shim.so ${FLATPAK_DEST}/lib/libflatpakdevshim.so
 
-ln -s ${FLATPAK_DEST}/extra/WireframeSketcher/WireframeSketcher ${FLATPAK_DEST}/bin/WireframeSketcher
+mkdir ${FLATPAK_DEST}/bin
+ln -s ${FLATPAK_DEST}/WireframeSketcher/WireframeSketcher ${FLATPAK_DEST}/bin/WireframeSketcher
 
 desktop-file-edit --set-key=Exec --set-value=WireframeSketcher --set-key=TryExec --set-value=WireframeSketcher ${FLATPAK_DEST}/share/applications/wireframesketcherstudio.desktop 
+
+INI_FILE=${FLATPAK_DEST}/WireframeSketcher/WireframeSketcher.ini
+
+sed -i -e '/osgi.configuration.area/d' $INI_FILE
+echo "-Dosgi.configuration.area=@user.home/.var/app/com.wireframesketcher.WireframeSketcher/WireframeSketcher/configuration" >> $INI_FILE
+echo "--patch-module=java.base=/app/flatpak-dev-shim.jar" >> $INI_FILE
+echo "-Dsun.boot.library.path=/app/lib" >> $INI_FILE
