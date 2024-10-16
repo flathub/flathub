72a51ad49e63c42462f5edf9b51b86cf8b1c87f2
Sai Karthik <kskarthik@disroot.org>
2024-10-16 16:09:11 +0530

    check for flathub repo before building

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak-builder build io.frappe.books.yml \
 	 --install-deps-from=flathub \
 	 --force-clean \
