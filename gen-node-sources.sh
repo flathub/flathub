3fb01c2e2327fcc73cae87f61d0412e0ade83826
Sebastian Grabowski <sebastian@grabel.de>
2024-10-18 21:23:00 +0200

    More wip

--- gen-node-sources.sh
+++ gen-node-sources.sh
@@ -1,4 +1,4 @@
 # Will not work. This is more of just a note for later.
 
 flatpak-node-generator -o packages-panmirror.json yarn https://raw.githubusercontent.com/quarto-dev/quarto/refs/heads/release/rstudio-cherry-blossom/yarn.lock
-flatpak-node-generator -o packages-rstudio.json node https://github.com/rstudio/rstudio/blob/main/src/node/desktop/package-lock.json
+flatpak-node-generator -o packages-rstudio.json npm https://github.com/rstudio/rstudio/blob/v2024.09.0%2B375/src/node/desktop/package-lock.json
