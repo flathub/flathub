839c65881df0d97dfa6f1fec3ad83244e4dcb917
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2024-12-02 14:56:07 -0600

    use actual .desktop file

--- apply_extra.sh
+++ apply_extra.sh
@@ -8,24 +8,14 @@ FLATPAK_ID=com.tracktion.Waveform13
 mkdir -p deb-package
 ar p waveform13.deb data.tar.gz | tar -zxf - -C deb-package
 
-# Move to export/share
+# Move executable to root dir
+mv deb-package/usr/bin/Waveform13 Waveform13
+
+# Export necessary share items
 mkdir -p export/share
-mv deb-package/usr/bin/Waveform13 waveform13
-mv deb-package/usr/share/{applications,doc,mime} export/share/
+mv deb-package/usr/share/{doc,mime} export/share/
+rename "waveform13" "${FLATPAK_ID}" export/share/mime/packages/waveform13.xml
 
-# Icon
-mkdir -p export/share/icons/hicolor/512x512/apps
-mv deb-package/usr/share/pixmaps/* export/share/icons/hicolor/512x512/apps
-
-rename "waveform13" "${FLATPAK_ID}" export/share/{applications,mime/packages,icons/hicolor/*/*}/waveform13.*
-
-# Desktop file
-desktop-file-edit \
-    --set-key="Exec" --set-value="waveform %U" \
-    --set-key="Icon" --set-value="${FLATPAK_ID}" \
-    --set-key="Categories" --set-value="AudioVideo;AudioVideoEditing;" \
-    --set-key="X-Flatpak-RenamedFrom" --set-value="waveform13.desktop;" \
-    "export/share/applications/${FLATPAK_ID}.desktop"
 
 # Download manager
 ar p tracktion-download-manager.deb data.tar.xz | tar -xJf - -C deb-package
