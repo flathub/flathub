0d9d0ae8e6e1404b62e754b94d3d8c8e777f27f1
Alex Ballas <alex@ballas.org>
2024-12-14 23:15:33 +0200

    Add app.go2tv.go2tv

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flatpak Go2TV
+
+![](https://i.imgur.com/Ga3hLJM.gif)
---
+++ build.sh
@@ -0,0 +1,1 @@
+flatpak-builder --force-clean --disable-cache --user --install-deps-from=flathub --repo=repo --install builddir app.go2tv.go2tv.yml
