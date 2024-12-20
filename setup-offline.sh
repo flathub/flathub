b0a8be99b0104233501fa5ba76ae776981586ffe
Alfian Losari <alfianlosari@gmail.com>
2024-12-20 15:01:09 +0700

    Initial Commit

---
+++ setup-offline.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+mkdir .build/repositories
+cd .build/repositories
+mkdir ./adwaita-swift-3f0bf4f5
+cp -r ../checkouts/adwaita-swift/.git/* ./adwaita-swift-3f0bf4f5
+mkdir ./async-http-client-afefb790
+cp -r ../checkouts/async-http-client/.git/* ./async-http-client-afefb790
+mkdir ./ChatGPTSwift-c0f61760
+cp -r ../checkouts/ChatGPTSwift/.git/* ./ChatGPTSwift-c0f61760
+mkdir ./CSQLite-ccd351fd
+cp -r ../checkouts/CSQLite/.git/* ./CSQLite-ccd351fd
+mkdir ./GPTEncoder-2eb21219
+cp -r ../checkouts/GPTEncoder/.git/* ./GPTEncoder-2eb21219
+mkdir ./levenshtein-transformations-11d2c005
+cp -r ../checkouts/levenshtein-transformations/.git/* ./levenshtein-transformations-11d2c005
+mkdir ./meta-sqlite-d4990edc
+cp -r ../checkouts/meta/.git/* ./meta-sqlite-d4990edc
+mkdir ./meta-e44c5837
+cp -r ../checkouts/meta/.git/* ./meta-e44c5837
+mkdir ./meta-sqlite-d4990edc
+cp -r ../checkouts/meta-sqlite/.git/* ./meta-sqlite-d4990edc
+mkdir ./SQLite.swift-bde2929d
+cp -r ../checkouts/SQLite.swift/.git/* ./SQLite.swift-bde2929d
+mkdir ./swift-algorithms-bf5a01cd
+cp -r ../checkouts/swift-algorithms/.git/* ./swift-algorithms-bf5a01cd
+mkdir ./swift-atomics-7429e549
+cp -r ../checkouts/swift-atomics/.git/* ./swift-atomics-7429e549
+mkdir ./swift-collections-9a58d5cf
+cp -r ../checkouts/swift-collections/.git/* ./swift-collections-9a58d5cf
+mkdir ./swift-http-types-ddff8b60
+cp -r ../checkouts/swift-http-types/.git/* ./swift-http-types-ddff8b60
+mkdir ./swift-log-ba8887eb
+cp -r ../checkouts/swift-log/.git/* ./swift-log-ba8887eb
+mkdir ./swift-nio-transport-services-195621bb
+cp -r ../checkouts/swift-nio/.git/* ./swift-nio-transport-services-195621bb
+mkdir ./swift-nio-extras-ee9c8e34
+cp -r ../checkouts/swift-nio/.git/* ./swift-nio-extras-ee9c8e34
+mkdir ./swift-nio-a9bb6d62
+cp -r ../checkouts/swift-nio/.git/* ./swift-nio-a9bb6d62
+mkdir ./swift-nio-http2-440d95c6
+cp -r ../checkouts/swift-nio/.git/* ./swift-nio-http2-440d95c6
+mkdir ./swift-nio-ssl-3eddd832
+cp -r ../checkouts/swift-nio/.git/* ./swift-nio-ssl-3eddd832
+mkdir ./swift-nio-extras-ee9c8e34
+cp -r ../checkouts/swift-nio-extras/.git/* ./swift-nio-extras-ee9c8e34
+mkdir ./swift-nio-http2-440d95c6
+cp -r ../checkouts/swift-nio-http2/.git/* ./swift-nio-http2-440d95c6
+mkdir ./swift-nio-ssl-3eddd832
+cp -r ../checkouts/swift-nio-ssl/.git/* ./swift-nio-ssl-3eddd832
+mkdir ./swift-nio-transport-services-195621bb
+cp -r ../checkouts/swift-nio-transport-services/.git/* ./swift-nio-transport-services-195621bb
+mkdir ./swift-numerics-d936ec6c
+cp -r ../checkouts/swift-numerics/.git/* ./swift-numerics-d936ec6c
+mkdir ./swift-openapi-async-http-client-6862762a
+cp -r ../checkouts/swift-openapi-async-http-client/.git/* ./swift-openapi-async-http-client-6862762a
+mkdir ./swift-openapi-runtime-c68fe40f
+cp -r ../checkouts/swift-openapi-runtime/.git/* ./swift-openapi-runtime-c68fe40f
+mkdir ./swift-openapi-urlsession-03c02701
+cp -r ../checkouts/swift-openapi-urlsession/.git/* ./swift-openapi-urlsession-03c02701
+mkdir ./swift-system-5815d4b7
+cp -r ../checkouts/swift-system/.git/* ./swift-system-5815d4b7
