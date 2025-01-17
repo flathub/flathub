1c8aa0037f7ad0ee0988b7400ebcc0e340719d85
Leonardo Cossutta <leonardo.cossutta@gmail.com>
2025-01-17 09:01:50 +0100

    remove script and do not use pulseaudio

--- compute-checksum.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/Omniaevo/mqtt5-explorer/releases/download/v1.16.1/com.omniaevo.mqtt5-explorer.tar.gz
-sha256sum com.omniaevo.mqtt5-explorer.tar.gz
-
-rm com.omniaevo.mqtt5-explorer.tar.gz
