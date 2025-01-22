ae1392e9c66ac273f9ada71afa0736ad1f4780e3
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-01-22 09:26:32 +0000

    Removed PTYSHELL as is ignored by app yet

--- freelens-launch.sh
+++ freelens-launch.sh
@@ -4,7 +4,6 @@ EXTRA_ARGS+=(
   "--ozone-platform-hint=auto"
 )
 
-export PTYSHELL=/bin/bash
 export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
 
 exec zypak-wrapper "/app/Freelens/freelens" "${EXTRA_ARGS[@]}" "$@"
