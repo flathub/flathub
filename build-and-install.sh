42697cd5202749ddba1a32bda4c44364c1f3a6de
SuperSonic16 <25761885+thesupersonic16@users.noreply.github.com>
2025-03-04 02:20:39 +1100

    Update tag to 8.0.0-beta2

--- build-and-install.sh
+++ build-and-install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-flatpak-builder --user --install --force-clean --install-deps-from=flathub builddir io.github.hedge_dev.hedgemodmanager.yml
+flatpak-builder --user --install --force-clean --install-deps-from=flathub --repo repo builddir io.github.hedge_dev.hedgemodmanager.yml
