b274f9cd0117037b1f35f09552889a9feab94285
Adrian Edwards <adrian@adriancedwards.com>
2025-03-09 21:14:23 -0400

    fix lack of spaces-in-filepaths handling in the npm sources script

--- scripts/npm-deps.sh
+++ scripts/npm-deps.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # pip install 
-git clone https://github.com/flatpak/flatpak-builder-tools
+# This is a temporary workaround for https://github.com/flatpak/flatpak-builder-tools/issues/445
+# once https://github.com/flatpak/flatpak-builder-tools/pull/446 is merged, replace the below line with
+# git clone https://github.com/flatpak/flatpak-builder-tools
+git clone https://github.com/MoralCode/flatpak-builder-tools --single-branch --branch handle-spaces
 cd flatpak-builder-tools/node
 pip install .
 cd ../../
