361fcc81ad533107dd88a84ec1284ccc22a6d99b
Jo Carllyle <96739684+JamesBrosy@users.noreply.github.com>
2025-05-10 22:31:53 +0800

    support input method framework

--- dingtalk.sh
+++ dingtalk.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+if [[ "$XMODIFIERS" =~ fcitx ]]; then
+    [ -z "$QT_IM_MODULE" ] && export QT_IM_MODULE=fcitx
+    [ -z "$GTK_IM_MODULE" ] && export GTK_IM_MODULE=fcitx
+elif [[ "$XMODIFIERS" =~ ibus ]]; then
+    [ -z "$QT_IM_MODULE" ] && export QT_IM_MODULE=ibus
+    [ -z "$GTK_IM_MODULE" ] && export GTK_IM_MODULE=ibus
+fi
 export QT_PLUGIN_PATH=/app/extra/dingtalk:$QT_PLUGIN_PATH
 cd /app/extra/dingtalk || exit 1
 preload_libs="./libgbm.so ./plugins/dtwebview/libcef.so "
