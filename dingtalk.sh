29ad691a91faf09c81c915c2f1279c141c8063c2
Jo Carllyle <96739684+JamesBrosy@users.noreply.github.com>
2025-05-12 17:45:35 +0800

    Update dingtalk.sh
    add env QT_QPA_PLATFORM

--- dingtalk.sh
+++ dingtalk.sh
@@ -6,6 +6,7 @@ elif [[ "$XMODIFIERS" =~ ibus ]]; then
     [ -z "$QT_IM_MODULE" ] && export QT_IM_MODULE=ibus
     [ -z "$GTK_IM_MODULE" ] && export GTK_IM_MODULE=ibus
 fi
+export QT_QPA_PLATFORM=xcb
 export QT_PLUGIN_PATH=/app/extra/dingtalk:$QT_PLUGIN_PATH
 cd /app/extra/dingtalk || exit 1
 ./com.alibabainc.dingtalk "$@"
