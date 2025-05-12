c70dfc2dbf4af40afd46ce781d7f316e523439f1
Jo Carllyle <96739684+JamesBrosy@users.noreply.github.com>
2025-05-12 17:33:07 +0800

    Update dingtalk.sh
    delete useless preload libs.

--- dingtalk.sh
+++ dingtalk.sh
@@ -8,6 +8,4 @@ elif [[ "$XMODIFIERS" =~ ibus ]]; then
 fi
 export QT_PLUGIN_PATH=/app/extra/dingtalk:$QT_PLUGIN_PATH
 cd /app/extra/dingtalk || exit 1
-preload_libs="./libgbm.so ./plugins/dtwebview/libcef.so "
-
-LD_PRELOAD="${preload_libs}" ./com.alibabainc.dingtalk "$@"
+./com.alibabainc.dingtalk "$@"
