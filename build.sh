ad363d8457f23b820eaf03db4fabdc640b9106b2
Hunter Perrin <hperrin@gmail.com>
2025-05-25 13:39:59 -0700

    Add com.hperrin.StreamOverlay

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+flatpak run org.flatpak.Builder --force-clean --sandbox --user --install --install-deps-from=flathub --ccache --mirror-screenshots-url=https://dl.flathub.org/media/ --repo=repo build/ com.hperrin.StreamOverlay.yml "$@"
---
+++ clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm -rf .flatpak-builder/ build/ repo/
