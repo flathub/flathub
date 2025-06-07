984450656b4196a3895cb3b78b406853531aec62
bbhtt <bbhtt.zn0i8@slmail.me>
2025-06-07 08:03:39 +0530

    merge: Explicitly check if a target repo already exists in the org

--- .github/actions/merge/entrypoint.py
+++ .github/actions/merge/entrypoint.py
@@ -176,6 +176,22 @@ def main():
 
     print(f"Detected {appid} as appid from {manifest_file}")
 
+    try:
+        org.get_repo(appid)
+        print(
+            f"Repository {appid} already exists in the flathub organisation, exiting."
+        )
+        sys.exit(1)
+    except github.GithubException as err:
+        if err.status == 404:
+            print(
+                f"Repository {appid} does not exist in the flathub organisation, continuing."
+            )
+            pass
+        else:
+            print(f"Unexpected error while checking for repository {appid}: {err}")
+            raise
+
     print("Creating new repo on Flathub")
     repo = org.create_repo(appid)
     time.sleep(5)
