95ef500b9860cb863d32722e08968f69ee1b02f3
bbhtt <bbhtt.zn0i8@slmail.me>
2025-08-09 10:16:21 +0530

    domain_from_appid: Fix the script and post more detailed comment

--- .github/scripts/domain_from_appid.py
+++ .github/scripts/domain_from_appid.py
@@ -1,41 +1,56 @@
+# ONLY handle appids that need to use `org.flathub.VerifiedApps.txt`
+
 import sys
+import re
 from publicsuffixlist import PublicSuffixList
 
-CODE_HOSTS = (
+LOGINS = (
+    "com.github.",
+    "com.gitlab.",
     "io.github.",
-    "io.frama.",
     "io.gitlab.",
-    "page.codeberg.",
-    "io.sourceforge.",
-    "net.sourceforge.",
     "org.gnome.gitlab.",
-    "org.freedesktop.gitlab.",
-    "site.srht.",
+    "org.gnome.World.",
+    "org.gnome.design",
+    "org.kde.",
+    "org.gnome.",
 )
+
 
 def demangle(name: str) -> str:
     if name.startswith("_"):
         name = name[1:]
     return name.replace("_", "-")
 
-def get_domain(appid: str) -> str | None:
-    if appid.startswith(CODE_HOSTS) or appid.count(".") < 2:
-        return None
 
-    if appid.startswith("org.gnome.") and not appid.startswith("org.gnome.gitlab."):
-        return "gnome.org"
-    elif appid.startswith("org.kde."):
-        return "kde.org"
-    elif appid.startswith("org.freedesktop.") and not appid.startswith("org.freedesktop.gitlab."):
-        return "freedesktop.org"
+def get_domain(appid: str) -> str | None:
+    if appid.startswith(LOGINS) or appid.count(".") < 2:
+        return None
+    elif appid.startswith(
+        ("io.frama.", "page.codeberg.", "io.sourceforge.", "net.sourceforge.")
+    ):
+        tld, domain, name = appid.split(".")[0:3]
+        name = demangle(name)
+        if domain == "sourceforge":
+            return f"{name}.{domain}.io".lower()
+        else:
+            return f"{name}.{domain}.{tld}".lower()
+    elif appid.startswith(("io.sourceforge.", "net.sourceforge.")):
+        [tld, domain, projectname] = appid.split(".")[0:3]
+        projectname = demangle(projectname)
+        return f"{projectname}.{domain}.io".lower()
     else:
         fqdn = ".".join(reversed(appid.split("."))).lower()
         psl = PublicSuffixList()
         if psl.is_private(fqdn):
             return demangle(psl.privatesuffix(fqdn))
         else:
-            return ".".join(reversed([demangle(i) for i in appid.split(".")[:-1]])).lower()
+            return ".".join(
+                reversed([demangle(i) for i in appid.split(".")[:-1]])
+            ).lower()
+
 
 if __name__ == "__main__":
-    appid = sys.argv[1]
-    print(get_domain(appid))
+    # PR title as input "(Aa)dd com.foo.bar"
+    appid = re.sub(r"^\s*add\s+", "", sys.argv[1], flags=re.IGNORECASE)
+    print(get_domain(appid).strip())
