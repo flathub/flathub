584515047b25b8161c7c5165ac1ccab6cebc4a1d
Tulip Blossom <tulilirockz@proton.me>
2025-08-15 01:27:48 -0300

    bazaar: integrate rust builds
    Signed-off-by: Tulip Blossom <tulilirockz@proton.me>

--- update-deps.sh
+++ update-deps.sh
@@ -3,6 +3,18 @@
 set -xeuo pipefail
 
 GLYCIN_TARGET_VERSION="${1:-1.2.3}"
+BAZAAR_TARGET_VERSION="${2:-0.3.0}"
+
+BAZAAR_LOCK_FILE_DIR=$(mktemp -d)
+trap 'rm -r "${BAZAAR_LOCK_FILE_DIR}"' EXIT
+curl -s -L "https://github.com/kolunmi/bazaar/archive/${BAZAAR_TARGET_VERSION}.tar.gz" | tar xzf - -C "${BAZAAR_LOCK_FILE_DIR}"
+
+podman run --rm -it \
+  -v .:/tmp/build:Z \
+  -v "${BAZAAR_LOCK_FILE_DIR}:${BAZAAR_LOCK_FILE_DIR}:Z" \
+  --pull newer \
+  docker.io/library/python:latest \
+  sh -c "pip install aiohttp toml && /tmp/build/flatpak-builder-tools/cargo/flatpak-cargo-generator.py ${BAZAAR_LOCK_FILE_DIR}/bazaar-${BAZAAR_TARGET_VERSION}/bazaar-ui/Cargo.lock -o /tmp/build/bazaar-cargo-sources.json"
 
 GLYCIN_LOCK_FILE_DIR=$(mktemp -d)
 trap 'rm -r "${GLYCIN_LOCK_FILE_DIR}"' EXIT
