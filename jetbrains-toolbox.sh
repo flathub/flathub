810a6fa08abe933f1cf7e1ebcaacdcfe94c7563e
dreamyukii <49896401+dreamyukii@users.noreply.github.com>
2025-08-29 22:20:35 +0700

    remove .config at jetbrains-toolbox.sh

--- jetbrains-toolbox.sh
+++ jetbrains-toolbox.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec env XDG_CONFIG_HOME="$HOME/.config" XDG_DATA_HOME="$HOME/.local/share" /app/extra/toolbox/bin/jetbrains-toolbox "$@" 
+exec env XDG_DATA_HOME="$HOME/.local/share" /app/extra/toolbox/bin/jetbrains-toolbox "$@" 
