ac6fd60a232f43ee897bee1b20ed0ec84ce8bff8
Mehdi <mah.fat@gmail.com>
2025-09-14 14:17:53 +0330

    fixes

--- sshpilot-launcher.sh
+++ sshpilot-launcher.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-export PYTHONPATH="/app/lib/python3/site-packages:${PYTHONPATH}"
-#export TERM="${TERM:-xterm-256color}"
-#export SHELL="${SHELL:-/bin/bash}"
 export SSHPILOT_FLATPAK=1
-exec python3 -m sshpilot.main "${@}"
+exec python3 -m sshpilot.main "$@"
