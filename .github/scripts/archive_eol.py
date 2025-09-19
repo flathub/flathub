9c677dc700ae69a7bbf147318a93fa0b35727a58
bbhtt <bbhtt.zn0i8@slmail.me>
2025-09-19 19:10:24 +0530

    Exclude org.nicotine_plus.Nicotine from EOL action

--- .github/scripts/archive_eol.py
+++ .github/scripts/archive_eol.py
@@ -128,6 +128,8 @@ def main() -> None:
         "com.yubico.yubioath",
         "com.icanblink.blink",
         "com.github.wwmm.pulseeffects",
+        # beta branch is eol
+        "org.nicotine_plus.Nicotine",
     }
 
     stable = get_eol_refs("x86_64", "flathub") | get_eol_refs("aarch64", "flathub")
