27aecd7885a963918b63bcd687005c07c9998477
Måns Fällman <mans.fallman@gmail.com>
2025-10-14 12:13:34 +0200

    update dependencies

---
+++ build-flatpak.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Make sure script is started from repo root.
+if [ "$(pwd)" != "$(git rev-parse --show-toplevel)" ]; then
+    echo -e '\033[31m(ERROR)\033[0m: Script called from wrong dir. Please start from the root of the repository'
+    exit 1
+fi
+
+# python3 -m flatpak_pip_generator -r requirements.txt --yaml --ignore-pkg pyclipper,PyGObject,pycairo --runtime='org.gnome.Sdk/49' --ignore-errors --output flatpak/python3-requirements
+
+flatpak run org.flatpak.Builder \
+    --force-clean --sandbox --user --install --ccache \
+    --install-deps-from=flathub \
+    --mirror-screenshots-url=https://dl.flathub.org/media/ \
+    --repo=repo builddir flatpak/com.barebaric.rayforge.yml
