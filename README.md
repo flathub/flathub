82385c481989314d9dff0093237d699d060988e7
Anlor <chenxiex@outlook.com>
2025-11-04 17:26:08 +0800

    Add cn.wps.wps_365

---
+++ README.md
@@ -0,0 +1,16 @@
+## WPS 365 flatpak
+
+[English](README.md)|[中文](README_CN.md)
+
+### Usage
+Download `.flatpak` bundles from [https://github.com/chenxiex/cn.wps.wps_365/releases/latest](https://github.com/chenxiex/cn.wps.wps_365/releases/) to install.
+
+If you’re concerned about potential privacy issues, you can disable network access by running: `flatpak --user override cn.wps.wps_365 --unshare=network`.
+
+### Tips
+- The application will launch "wps cloud storage", and create an icon on system tray. If you uncheck "start with wps" in the settings of the tool, it will still be launched without the tray icon, making it impossible to gracefully exit the tool. In that case you will need `flatpak kill cn.wps.wps_365` to completely close the application.
+- When the "Window Management Mode" is set to "Integrated Mode", the component’s desktop files cannot be used to launch the application. This is an upstream bug. If you want to launch the application using a component’s desktop file, you must switch the "Window Management Mode" to "Multi-Component Mode" in settings. If you can't launch the gui, you can also edit or create `~/.var/app/cn.wps.wps_365/config/Kingsoft/Office.conf` with
+    ```
+    [6.0]
+    wpsoffice\Application%20Settings\AppComponentMode=prome_independ
+    ```
---
+++ apply_extra.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+set -e
+shopt -s failglob
+FLATPAK_ID="${FLATPAK_ID:-cn.wps.wps_365}"
+CARCH="$(uname -m)"
+
+mkdir -p export/share/
+
+bsdtar --to-stdout -xf wps-office.deb data.tar.xz | bsdtar -xf -
+
+# Move shared files to export directory
+mv usr/share/{icons,applications,mime} export/share/
+
+# Rename icons, desktop files, and mime files to use Flatpak ID
+YEAR_SUFFIX=2023
+rename --no-overwrite "wps-office-" "${FLATPAK_ID}." export/share/{icons/hicolor/*/*,applications}/wps-office-*.*
+rename --no-overwrite "wps-office${YEAR_SUFFIX}-" "${FLATPAK_ID}." export/share/icons/hicolor/*/*/wps-office${YEAR_SUFFIX}-*.*
+rename --no-overwrite "custom-wps-office" "${FLATPAK_ID}" export/share/mime/packages/custom-wps-office.xml
+rename --no-overwrite "xiezuo" "${FLATPAK_ID}.xiezuo" export/share/{icons/hicolor/*/*,applications}/xiezuo.*
+
+# Edit .desktop files to adjust Exec and Icon entries
+for a in wps wpp et pdf prometheus xiezuo; do
+    desktop_file="export/share/applications/${FLATPAK_ID}.$a.desktop"
+    appbin="$a %F"
+    appicon="${FLATPAK_ID}.${a}main"
+    renamedfrom="wps-office-$a.desktop"
+    case "$a" in
+        pdf)
+            appbin="wpspdf %F"
+        ;;
+        prometheus)
+            appbin="wps %F"
+            appicon="${FLATPAK_ID}.k${a}"
+            # Use this as the main .desktop file for the Flatpak
+            new_desktop_file="$(dirname $desktop_file)/${FLATPAK_ID}.desktop"
+            mv $desktop_file $new_desktop_file
+            desktop_file=$new_desktop_file
+        ;;
+        xiezuo)
+            appbin="/app/extra/opt/xiezuo/xiezuo --no-sandbox --disable-gpu-sandbox --disable-setuid-sandbox --package-format=deb %U"
+            appicon="${FLATPAK_ID}.xiezuo"
+            renamedfrom="xiezuo.desktop"
+        ;;
+    esac
+    sed -i 's|Categories=.*|&Office;|' "$desktop_file"
+    desktop-file-edit \
+        --set-key="Exec" --set-value="$appbin" \
+        --set-key="Icon" --set-value="$appicon" \
+        --set-key="X-Flatpak-RenamedFrom" --set-value="$renamedfrom" \
+        "$desktop_file"
+done
+
+# Edit mime XML files to adjust generic-icon names
+sed -i "s/generic-icon name=\"wps-office-/icon name=\"${FLATPAK_ID}./g" "export/share/mime/packages/${FLATPAK_ID}".xml
+
+# Fix hardcoded /opt paths in binaries
+sed -i 's|/opt|/app/extra/opt|' usr/bin/*
+
+# use system libraries
+rm opt/kingsoft/wps-office/office6/lib{jpeg,stdc++}.so*
+if [[ "$CARCH" = "aarch64" ]]; then
+    rm opt/kingsoft/wps-office/office6/addons/cef/libm.so*
+    rm opt/kingsoft/wps-office/office6/libfreetype.so*
+fi
+
+# fix input method
+sed -i '2i [[ "$XMODIFIERS" == "@im=fcitx" ]] && export QT_IM_MODULE=fcitx' \
+    usr/bin/{wps,wpp,et,wpspdf}
+
+rm -r wps-office.deb
