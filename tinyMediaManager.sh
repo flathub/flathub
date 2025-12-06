81cef28165a9feb2ecf59d39e05f193cb216b0b0
Manuel Laggner <manuel.laggner@gmail.com>
2025-12-06 15:49:39 +0100

    added arm64 build

--- tinyMediaManager.sh
+++ tinyMediaManager.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 cd /app/tmm
-exec /app/tmm/tinyMediaManager "$@"
+
+# Detect architecture and launch appropriate executable
+ARCH=$(uname -m)
+case "$ARCH" in
+    aarch64|arm64)
+        exec /app/tmm/tinyMediaManager-arm "$@"
+        ;;
+    x86_64|amd64)
+        exec /app/tmm/tinyMediaManager "$@"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
