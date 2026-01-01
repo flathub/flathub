6a048ff81d359fb76b1087a6ae0edeadb6110d54
imikado <imikado@gmail.com>
2026-01-01 16:31:06 +0100

    remove files as expected

--- buildBundle.sh
+++
@@ -1,6 +0,0 @@
-flatpak-builder \
-  --force-clean \
-  --repo=repo \
-  --mirror-screenshots-url=https://dl.flathub.org/media \
-  build-dir \
-  org.dupot.savethesheep.yml
--- check.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -xe
-#flatpak run org.flatpak.Builder --force-clean --sandbox --user --install --install-deps-from=flathub --ccache --mirror-screenshots-url=https://dl.flathub.org/media/ --repo=repo builddir org.dupot.savethesheep.yml
-flatpak run --command=flatpak-builder-lint org.flatpak.Builder manifest org.dupot.savethesheep.yml
-flatpak run --command=flatpak-builder-lint org.flatpak.Builder --exceptions repo repo
