6d3708e23cd1c67715678d0209dd74a3b83df01a
Jarosław Dyszczyński <jarek@dyszczynski.pl>
2026-03-03 10:28:47 +0100

    removing start script

--- ksef_wrapper.sh
+++
@@ -1,2 +0,0 @@
-# wrapper kept for future use, currently just a simple pass-through to the main binary. This allows us to set environment variables or do other setup in the future if needed without changing the Flatpak manifest.
-exec /app/bin/KsefInvoice "$@"
