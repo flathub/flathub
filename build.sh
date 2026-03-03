15b9292052e33d497e5f599f6327d12d9a5f204a
Marin <marin-m@users.noreply.github.com>
2026-03-03 06:48:00 +0100

    Add re.fossplant.vmlinux-to-elf

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Make errors fatal, print commands
+set -ex
+
+# Install the required Flatpak runtime and SDK
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub --user org.gnome.Sdk//49 -y
+flatpak install flathub --user org.gnome.Platform//49 -y
+
+# Build the Flathub package
+rm -rf target/ # Don't copy all the planet into the Flatpak build dir
+rm -rf repo/
+PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig/ flatpak-builder --install repo re.fossplant.vmlinux-to-elf.json --user -y
