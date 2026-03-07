a35ea7e4a38da3472a1d231e05a88afdd60537a9
Zbynek Stava <zbynek.stava@gmail.com>
2026-03-07 11:31:39 +0100

    removed source code from PR, added source code reference to original gitlab repo

--- eu.capybaracode.FilenameNinja/src/filename_ninja/__init__.py
+++
@@ -1,7 +0,0 @@
-# filename_ninja package
-
-__all__ = ["MainConfig", "FileInfo", "FilenameNinjaApp"]
-
-from .config import MainConfig
-from .file_info import FileInfo
-from .ui.main_window import FilenameNinjaApp
