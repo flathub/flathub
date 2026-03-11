08f722d2872408d819e01691faccf072c2e3976f
Christoffer <ckp@kragh.dev>
2026-03-11 23:08:53 +0100

    Use builder tool pnpm based on nukeop's fork

--- prepare.sh
+++ prepare.sh
@@ -9,30 +9,22 @@ if ! command -v flatpak-node-generator &>/dev/null; then
     echo "Installing flatpak-node-generator via pipx..."
     # Make sure ~/.local/bin is on PATH for this session
     export PATH="${HOME}/.local/bin:${PATH}"
-    pipx install flatpak-node-generator
+    #TODO - replace with url when pnpm implemented
+    pipx install git+ssh://git@github.com/tonur/flatpak-builder-tools.git#subdirectory=node --force
     if ! command -v flatpak-node-generator &>/dev/null; then
         echo "ERROR: flatpak-node-generator still not found after install."
         exit 1
     fi
 fi
 
-
 if [ ! -d "$UPSTREAM_DIR" ] ; then
-    git clone https://github.com/stoatchat/for-desktop.git --depth=1 --branch=v1.1.12 "$UPSTREAM_DIR"
+    git clone https://github.com/stoatchat/for-desktop.git --depth=1 --branch=v1.3.0 "$UPSTREAM_DIR"
 fi
 
 cd "$UPSTREAM_DIR"
 
-npm install --package-lock-only --lockfile-version 1
-pnpm make
+pnpm install
 
 cd "$SCRIPT_DIR"
 
-flatpak-node-generator --electron-node-headers npm "$UPSTREAM_DIR/package-lock.json" -o generated-sources.json
-# Replace the following line:
-# "ln -sf \"linux-x64@0.21.5\" \"bin/esbuild-current\""
-# With this line:
-# "ln -sf \"@esbuild/linux-x64@0.21.5\" \"bin/esbuild-current\""
-
-cp "$UPSTREAM_DIR/package-lock.json" "$SCRIPT_DIR/package-lock.json"
-cp -r "$UPSTREAM_DIR/.vite/" "$SCRIPT_DIR/.vite/"
+flatpak-node-generator pnpm "$UPSTREAM_DIR/pnpm-lock.yaml" -o generated-sources.json --electron-node-headers
