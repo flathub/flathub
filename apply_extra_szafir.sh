0019e13f4cc28314c7f88bee2be1020a58e1b56f
deno <>
2026-03-29 00:20:44 +0100

    remove copyrighted icons from apply_extra

--- apply_extra_szafir.sh
+++ apply_extra_szafir.sh
@@ -4,9 +4,3 @@ set -eu
 
 bsdtar -xf szafir_Linux.zip szafir_703.jar libCCGraphiteP11.2.0.5.6.so
 rm szafir_Linux.zip
-
-for size in 16 24 32 48 64; do
-	out_dir="/app/extra/export/share/icons/hicolor/${size}x${size}/apps"
-	install -d "$out_dir"
-	bsdtar -xOf szafir_703.jar "pl/com/kir/szafir/resources/image/icon-application${size}.png" > "$out_dir/pl.deno.kir.szafir.png"
-done
