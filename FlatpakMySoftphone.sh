b89be3b14487330c893b9edc93efb71aa4ad3cc1
realdev <realdev.company@gmail.com>
2026-04-13 13:58:18 +0200

    Add shared-modules submodule

--- FlatpakMySoftphone.sh
+++ FlatpakMySoftphone.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-APPDIR="/app/mysoftphone"
+APPDIR="/app/extra"
 
-export LD_LIBRARY_PATH="/app/lib:${APPDIR}/lib:${LD_LIBRARY_PATH:-}"
+export LD_LIBRARY_PATH="/app/lib:/app/lib64:${APPDIR}/lib:${LD_LIBRARY_PATH:-}"
 
 exec "${APPDIR}/MySoftphone" "$@"
