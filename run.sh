58d16287ab42f102d255df3d55a8e8b4b867d6b4
efzynx <usenpusen1@gmail.com>
2026-04-19 08:30:24 +0700

    flatpak: fix build paths and disable aarch64 build

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Wrapper ini menggunakan zypak-wrapper, yaitu sistem Flatpak khusus untuk menghindari bentrok 
-# antara sandbox Chromium bawaan Electron dan sandbox Flatpak.
-exec zypak-wrapper /app/main/truckers-tool-linux "$@"
