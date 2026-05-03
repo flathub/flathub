2356d7c5e6f66bc310d528eb0fa596df5e7e9320
SharkPlush <86890204+SharkPlush@users.noreply.github.com>
2026-05-04 01:24:11 +0300

    Fix 3 issues.
    https://github.com/flathub/flathub/pull/8547#discussion_r3178769932 - Fixed
    https://github.com/flathub/flathub/pull/8547#discussion_r3178770154 - Fixed
    https://github.com/flathub/flathub/pull/8547#discussion_r3178776594 - Fix (very happy about this one.)

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-exec zypak-wrapper /app/bin/ledger-live-desktop "$@"
+
+if [ ! $WAYLAND_DISPLAY ]; then 
+	exec zypak-wrapper /app/bin/ledger-live-desktop --ozone-platform=x11 "$@"
+else
+	exec zypak-wrapper /app/bin/ledger-live-desktop "$@"
+fi
