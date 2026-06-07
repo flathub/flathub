3d3c07d52dec3eb8e05d94c34bcb7e1d9146c23a
BlenderEngineer <82451249+BlenderEngineer@users.noreply.github.com>
2026-06-07 10:57:22 +0300

    Simplify launch script
    Removed check for game files and related error message.

--- launch.sh
+++ launch.sh
@@ -2,9 +2,5 @@
 # Flatpak entrypoint (installed as /app/bin/Sibir.sh).
 set -eu
 GAME_DIR="/app/opt/sibir"
-if [ ! -x "${GAME_DIR}/Sibir.x86_64" ]; then
-	echo "Sibir: game files missing. If this is an extra-data build, finish install from Flathub." >&2
-	exit 1
-fi
 cd "${GAME_DIR}"
 exec ./Sibir.x86_64 "$@"
