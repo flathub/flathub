c41c23b2df1459a830f905c8fb15f26ffa768087
OninesixY <theoninesixy@outlook.com>
2026-08-26 12:55:00 +0800

    io.qzz.OninesixY.HMCL

---
+++ .dev.buid_and_start.sh
@@ -0,0 +1,2 @@
+flatpak-builder --user --install --force-clean build-dir io.qzz.OninesixY.HMCL.yml
+flatpak run io.qzz.OninesixY.HMCL
---
+++ .dev.test.sh
@@ -0,0 +1,3 @@
+desktop-file-validate io.qzz.OninesixY.HMCL.desktop
+appstream-util validate io.qzz.OninesixY.HMCL.metainfo.xml
+flatpak-builder-lint manifest io.qzz.OninesixY.HMCL.yml
